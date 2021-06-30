6d43bef75d142f1d8ec7d11c94a876af4b5c4971
Oleh Shypula <olehshypulav@gmail.com>
2021-06-30 16:43:20 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,59 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(getCoinCombination())
+      .toBeInstanceOf(Array);
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
   });
 
   // write more tests here
+
+  it(`should return [1, 0, 0, 0] if amount = 1`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return [0, 1, 0, 0] if amount = 5`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return [1, 1, 0, 0] if amount = 6`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return [0, 0, 1, 0] if amount = 10`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [2, 1, 1, 0] if amount = 17`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return [0, 0, 0, 1] if amount = 25`, () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return [2, 1, 1, 1] if amount = 42`, () => {
+    expect(getCoinCombination(42))
+      .toEqual([2, 1, 1, 1]);
+  });
+
+  it(`should return [0, 0, 0, 2] if amount = 50`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should return [4, 0, 2, 3] if amount = 99`, () => {
+    expect(getCoinCombination(99))
+      .toEqual([4, 0, 2, 3]);
+  });
 });
