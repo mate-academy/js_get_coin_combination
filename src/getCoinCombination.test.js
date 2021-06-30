70dde45a8bddb8a55912c88e8eb11adc3da4ab8f
Alina Tymchenko <alina.timchenko01@gmail.com>
2021-06-30 14:54:08 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,17 +4,32 @@ describe(`Function 'getCoinCombination':`, () => {
   const getCoinCombination = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
-
+    expect(typeof getCoinCombination()).toStrictEqual('object');
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
   // write more tests here
+
+  it(`should return 1 penny if amount of coins = 1`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return 1 nickel if amount of coins = 5`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return 1 dime if amount of coins = 10`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return 1 quarter if amount of coins = 25`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
