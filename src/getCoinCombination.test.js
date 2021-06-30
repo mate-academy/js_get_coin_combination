ec06a03ae69c578fd9e366a6ece50239404a28d5
BohdanMakarov <bmakarov322@gmail.com>
2021-06-30 20:43:14 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,14 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(Array.isArray(getCoinCombination(100))).toBeTruthy();
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  // write more tests here
+  it(`should return [0, 0, 0, 0] if amount = 0`, () => {
+    expect(getCoinCombination()).toEqual([0, 0, 0, 0]);
+  });
 });
