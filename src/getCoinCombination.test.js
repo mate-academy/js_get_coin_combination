714d09b1afcb322e41b0413b6f008748e6cb0517
Dana Oliynyk <dana.victorivna23@gmail.com>
2021-07-01 14:44:30 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,30 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(getCoinCombination(1)).toBeInstanceOf(Array);
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  // write more tests here
+  it(`should return [2, 0, 0, 0] if amount = 2`, () => {
+    expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
+  });
+
+  it(`should return [0, 0, 1, 0] if amount = 10`, () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [4, 0, 2, 0] if amount = 24`, () => {
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
+  });
+
+  it(`should return [1, 0, 2, 1] if amount = 46`, () => {
+    expect(getCoinCombination(46)).toEqual([1, 0, 2, 1]);
+  });
+
+  it(`should return [0, 0, 0, 1] if amount = 100`, () => {
+    expect(getCoinCombination(100)).toEqual([0, 0, 0, 4]);
+  });
 });
