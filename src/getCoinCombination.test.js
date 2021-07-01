02499d58beebdee9420f1767dda5dff5da9d8793
XMODER-ua <sanya22secret@gmail.com>
2021-07-01 14:50:54 +0300

    work is done

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,22 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(getCoinCombination()).toBeInstanceOf(Array);
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  // write more tests here
+  it(`should return [1, 1, 0, 0] if amount = 6`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return [0, 0, 0, 1] if amount = 25`, () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return [2, 0, 1, 3] if amount = 87`, () => {
+    expect(getCoinCombination(87)).toEqual([2, 0, 1, 3]);
+  });
 });
