98b6255bf5a2d6e81aeeeb7ee2d037485f0b073d
OlhaZubko <110scof@gmail.com>
2021-07-03 23:35:06 +0300

    homework#_3

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,23 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(getCoinCombination()).toBeInstanceOf(Array);
   });
 
-  it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+  it(`should return [1, 0, 0, 0] if amount = 1`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
+  it(`should return [2, 1, 0, 0] if amount = 7`, () => {
+    expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
+  });
+
+  it(`should return [2, 0, 1, 0] if amount = 12`, () => {
+    expect(getCoinCombination(12)).toEqual([2, 0, 1, 0]);
+  });
+
+  it(`should return [2, 1, 0, 1] if amount = 32`, () => {
+    expect(getCoinCombination(32)).toEqual([2, 1, 0, 1]);
+  });
   // write more tests here
 });
