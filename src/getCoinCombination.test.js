4480bbc8eec132d4ab641431057d8b983e739d6a
annasisoian <anna.sisoian@gmail.com>
2021-07-04 17:21:18 +0300

    deleted redundant tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -28,24 +28,12 @@ describe(`Function 'getCoinCombination':`, () => {
     expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
   });
 
-  it(`should return [2, 0, 2, 15] if amount = 397`, () => {
-    expect(getCoinCombination(397)).toEqual([2, 0, 2, 15]);
-  });
-
   it(`should return [2, 0, 0, 0] if amount = 2`, () => {
     expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
   });
 
   it(`should return [1, 1, 0, 2] if amount = 56`, () => {
     expect(getCoinCombination(56)).toEqual([1, 1, 0, 2]);
-  });
-
-  it(`should return [2, 0, 1, 3] if amount = 87`, () => {
-    expect(getCoinCombination(87)).toEqual([2, 0, 1, 3]);
-  });
-
-  it(`should return [3, 0, 2, 7] if amount = 198`, () => {
-    expect(getCoinCombination(198)).toEqual([3, 0, 2, 7]);
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
