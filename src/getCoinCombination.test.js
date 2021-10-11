480fcdecaf6c23ae68629271bb9ee038b8bc5982
NikoLazur <lazur.niko@gmail.com>
2021-10-11 16:08:00 +0300

    Added solution for CoinCombination

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,12 +9,30 @@ describe(`Function 'getCoinCombination':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(getCoinCombination([]))
+      .toBeInstanceOf(Array);
   });
 
   it(`should return [0, 0, 0, 0] if amount = 0`, () => {
-
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  // write more tests here
+  it(`should return [0, 0, 0, 0] if amount = 0`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return minimum number of coins combination 
+    of the same value`, () => {
+    expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
+  });
+
+  it(`should return minimum number of coins combination 
+    of the same value`, () => {
+    expect(getCoinCombination(40)).toEqual([0, 1, 1, 1]);
+  });
+
+  it(`should return minimum number of coins combination 
+    of the same value`, () => {
+    expect(getCoinCombination(42)).toEqual([2, 1, 1, 1]);
+  });
 });
