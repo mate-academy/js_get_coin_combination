32aab61a19389a81d37295716637db9eb7970e92
NikoLazur <lazur.niko@gmail.com>
2021-10-12 16:49:39 +0300

    Added conditions for each tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -17,19 +17,19 @@ describe(`Function 'getCoinCombination':`, () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  it(`should return 1 penny, 1 nickel, 1 dime, 1 quarter`, () => {
+  it(`should return 1 penny, 1 nickel, 1 dime, 1 quarter if amount = '41`, () => {
     expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
   });
 
-  it(`should return 0 penny, 1 nickel, 1 dime, 1 quarter`, () => {
+  it(`should return 0 penny, 1 nickel, 1 dime, 1 quarter if amount = '40`, () => {
     expect(getCoinCombination(40)).toEqual([0, 1, 1, 1]);
   });
 
-  it(`should return 2 penny, 1 nickel, 1 dime, 1 quarter`, () => {
+  it(`should return 2 penny, 1 nickel, 1 dime, 1 quarter if amount = '42`, () => {
     expect(getCoinCombination(42)).toEqual([2, 1, 1, 1]);
   });
 
-  it(`should return 2 quarters`, () => {
+  it(`should return 2 quarters if amount = '50`, () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
