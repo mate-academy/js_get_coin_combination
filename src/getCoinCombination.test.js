dffaf9d960bd90a9f3aa21ec3cc4e688cab065fc
Volodymyr Polishchuk <rot010011@gmail.com>
2021-12-13 14:51:55 +0200

    tests are done

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,8 +1,60 @@
+'use strict';
+
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+  test(`Function should return [0, 0, 0, 0] if amount of cents = 0`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  test(`Function should return [1, 0, 0, 0] if amount of cents = 1`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  test(`Function should return [4, 0, 0, 0] if amount of cents = 4`, () => {
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+  });
+
+  test(`Function should return [0, 1, 0, 0] if amount of cents = 5`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
+  test(`Function should return [1, 1, 0, 0] if amount of cents = 6`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  test(`Function should return [4, 1, 0, 0] if amount of cents = 9`, () => {
+    expect(getCoinCombination(9))
+      .toEqual([4, 1, 0, 0]);
+  });
+
+  test(`Function should return [0, 0, 1, 0] if amount of cents = 10`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  test(`Function should return [1, 0, 1, 0] if amount of cents = 11`, () => {
+    expect(getCoinCombination(11))
+      .toEqual([1, 0, 1, 0]);
+  });
+
+  test(`Function should return [4, 0, 2, 0] if amount of cents = 24`, () => {
+    expect(getCoinCombination(24))
+      .toEqual([4, 0, 2, 0]);
+  });
+
+  test(`Function should return [0, 0, 0, 1] if amount of cents = 25`, () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
+  test(`Function should return [1, 0, 0, 1] if amount of cents = 26`, () => {
+    expect(getCoinCombination(26))
+      .toEqual([1, 0, 0, 1]);
   });
 });
