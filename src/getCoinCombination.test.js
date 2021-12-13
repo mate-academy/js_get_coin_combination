fab161d07a2ab315318f65beef00c337ecc88f6f
Iren Gorak <iren.horak@gmail.com>
2021-12-13 16:25:52 +0200

    Solution 3

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,8 +1,26 @@
+'use strict';
+
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
+  });
+
+  it('should be 1 penny,if 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should be 1 penny, 1 nickel if 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should be 2 penies, 1 nickel, 1 dime if 17 cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should be 2 quaters,if 50 cents', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
