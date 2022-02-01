9629e691f9df47bd144ef403c639067a99eaefc2
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-02-01 13:35:08 +0200

    final

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -6,9 +6,13 @@
  * @returns {number[]}
  */
 function getCoinCombination(cents) {
-  let currentAmount = cents;
+  let currentAmount = Math.trunc(cents);
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
+
+  if (cents < 0) {
+    return [cents];
+  }
 
   for (let i = 3; i >= 0; i--) {
     coins[i] = Math.floor(currentAmount / values[i]);
--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,32 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return [1, 1, 1, 1] when value equal to 41`, () => {
+    const result = getCoinCombination(41);
+
+    expect(result)
+      .toEqual([1, 1, 1, 1]);
+  });
+
+  it(`should return [cents] when value < 0`, () => {
+    const result = getCoinCombination(-3);
+
+    expect(result)
+      .toEqual([-3]);
+  });
+
+  it(`should return [0, 0, 0, 0] when value equal 0`, () => {
+    const result = getCoinCombination(0);
+
+    expect(result)
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should discard floating point`, () => {
+    const result = getCoinCombination(15.3);
+
+    expect(result)
+      .toEqual([0, 1, 1, 0]);
+  });
 });
