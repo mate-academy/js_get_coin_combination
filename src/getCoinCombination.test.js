cc63064651c23479339d010c80c4e14223a88eeb
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-02-02 18:05:19 +0200

    finalFixes

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -21,4 +21,32 @@ describe('getCoinCombination', () => {
     expect(result)
       .toEqual([0, 0, 0, 0]);
   });
+
+  it(`should return [1, 0, 0, 0] when value equal 1`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result)
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return [0, 1, 0, 0] when value equal 5`, () => {
+    const result = getCoinCombination(5);
+
+    expect(result)
+      .toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return [0, 0, 1, 0] when value equal 10`, () => {
+    const result = getCoinCombination(10);
+
+    expect(result)
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [0, 0, 0, 1] when value equal 25`, () => {
+    const result = getCoinCombination(25);
+
+    expect(result)
+      .toEqual([0, 0, 0, 1]);
+  });
 });
