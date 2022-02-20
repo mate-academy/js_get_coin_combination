6ce4eaafbab23d7dac7b0bd28f7786881e340963
v.savchenko <v.savchenko@polytech.software>
2022-02-20 21:21:22 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,22 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`should return 1 penny`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return 1 penny and 1 nickel`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should retuen 2 pennies + 1 nickel + 1 dime`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return 2 quarters`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
