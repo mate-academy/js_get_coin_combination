81e4ecdca8977719e8070f2edd9834f9c815f7c4
v.savchenko <v.savchenko@polytech.software>
2022-02-20 22:24:43 +0200

    update test names

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+describe("getCoinCombination", () => {
+  const { getCoinCombination } = require("./getCoinCombination");
 
   it(`should be declared`, () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
 
-  it(`should return 1 penny`, () => {
+  it(`should return pennies`, () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it(`should return 1 penny and 1 nickel`, () => {
+  it(`should return pennies and nickels pair`, () => {
     expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it(`should retuen 2 pennies + 1 nickel + 1 dime`, () => {
+  it(`should retuen pennies, nickels and dimes`, () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it(`should return 2 quarters`, () => {
+  it(`should return quarters`, () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
