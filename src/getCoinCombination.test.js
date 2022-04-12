ace746bf807560430e14b1824974feb611fea597
alinatkachuk <alinatkachuk555@gmail.com>
2022-04-12 21:28:41 +0300

    Solution2

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -15,4 +15,16 @@ describe('getCoinCombination', () => {
   it(`should be return [2, 0, 0, 0] if  cents = 2`, () => {
     expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
   });
+
+  it(`should be return [2, 1, 0, 0] if  cents = 7`, () => {
+    expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
+  });
+
+  it(`should be return [0, 0, 1, 0] if  cents = 10`, () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 10, 0]);
+  });
+
+  it(`should be return [2, 0, 0, 1] if  cents = 10`, () => {
+    expect(getCoinCombination(27)).toEqual([2, 0, 0, 1]);
+  });
 });
