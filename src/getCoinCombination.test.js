e39d8a6f33e48c42aaf6b0651ae60747809a3107
Sofia Drych <sofia.drych@gmail.com>
2022-04-12 15:34:58 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,44 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return an array`, () => {
+    expect(getCoinCombination(1)).toBeInstanceOf(Array);
+  });
+
+  it(`should return [0, 0, 0, 0] for cents = 0`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return [4, 0, 0, 0] for cents = 4`, () => {
+    expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
+  });
+
+  it(`should return [0, 1, 0, 0] for cents = 5`, () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return [4, 1, 0, 0] for cents = 9`, () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+  });
+
+  it(`should return [0, 0, 1, 0] for cents = 10`, () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [4, 0, 2, 0] for cents = 24`, () => {
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
+  });
+
+  it(`should return [0, 0, 0, 1] for cents = 25`, () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return [4, 0, 2, 1] for cents = 49`, () => {
+    expect(getCoinCombination(49)).toEqual([4, 0, 2, 1]);
+  });
+
+  it(`should return [0, 0, 0, 2] for cents = 50`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
