4026db199af4438a79983feaee919b0c90a261ac
alinatkachuk <alinatkachuk555@gmail.com>
2022-04-12 19:10:52 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,12 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should be return [0, 0, 0, 0] if  cents = 0`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should be return [2, 0, 0, 0] if  cents = 2`, () => {
+    expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
+  });
 });
