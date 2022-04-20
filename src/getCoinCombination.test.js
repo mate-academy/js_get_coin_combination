e88597a4a2d82b98c6dd2264e24c61874cf1d93a
Anton Volkov <qvadro8888@gmail.com>
2022-04-20 19:03:52 +0300

    Fixed solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,27 +7,27 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
 
-  it('should return [1, 0, 0, 0] when input value is (1)', () => {
+  it('should return [1, 0, 0, 0] when input value  = 1', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it('should return [1, 1, 0, 0] when input value is (6)', () => {
+  it('should return [1, 1, 0, 0] when input value = 6', () => {
     expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it('should return [2, 1, 1, 0] when input value is (17)', () => {
+  it('should return [2, 1, 1, 0] when input value = 17 ', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it('should return [0, 0, 0, 2] when input value is (50)', () => {
+  it('should return [0, 0, 0, 2] when input value = 50)', () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 
-  it('should return [2, 0, 2, 2] when input value is (72)', () => {
+  it('should return [2, 0, 2, 2] when input value = 72', () => {
     expect(getCoinCombination(72)).toEqual([2, 0, 2, 2]);
   });
 
-  it('should return [3, 0, 1, 3] when input value is (88)', () => {
+  it('should return [3, 0, 1, 3] when input value = 88', () => {
     expect(getCoinCombination(88)).toEqual([3, 0, 1, 3]);
   });
 });
