4af480d6b6128f87bb0fa3d537d7a0437bbb5b62
Maksym Hukovskyi <gukovskijmaksim64@gmail.com>
2022-06-28 12:40:13 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,34 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return an array`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return 1 penny and 1 nickel`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return 2 quaters and 1 nickel`, () => {
+    expect(getCoinCombination(55))
+      .toEqual([0, 1, 0, 2]);
+  });
+
+  it(`should return NaN when input is not digit`, () => {
+    expect(getCoinCombination('qwerty'))
+      .toEqual([NaN, NaN, NaN, NaN]);
+  });
+
+  it(`should return 1 pennie , 1 nickel and 1 dime`, () => {
+    expect(getCoinCombination(16))
+      .toEqual([1, 1, 1, 0]);
+  });
+
+  it(`should return 1 pennie, 1 nickel, 1 dime and 1 quater `, () => {
+    expect(getCoinCombination(41))
+      .toEqual([1, 1, 1, 1]);
+  });
 });
