21d1e96d5a826d5b662cf7735b488191ece7b6f4
krolich500grn <edu.izaytsev@gmail.com>
2022-07-04 12:26:39 +0300

    feat: input validation to throw error

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -6,6 +6,10 @@
  * @returns {number[]}
  */
 function getCoinCombination(cents) {
+  if (cents < 0 || typeof cents !== 'number' || isNaN(cents)) {
+    throw new Error('Input should be a non-negative integer');
+  };
+
   let currentAmount = cents;
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
