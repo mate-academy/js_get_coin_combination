ae7d6d45e45ffe2f163631dfe1b24a8db7917fac
Ivan Grekov <i.hrekov@smissltd.com>
2022-07-31 18:09:29 +0300

    feat: test for getCoinCombination function were implemented

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,8 +3,51 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+  it('zeros array if there is no argument', () => {
+    const result = getCoinCombination();
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it('zeros array if there is negative argument', () => {
+    const result = getCoinCombination(-1);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it('1 penny', () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it('1 nickel', () => {
+    const result = getCoinCombination(5);
+
+    expect(result).toEqual([0, 1, 0, 0]);
+  });
+
+  it('1 dime', () => {
+    const result = getCoinCombination(10);
+
+    expect(result).toEqual([0, 0, 1, 0]);
+  });
+
+  it('1 quarter', () => {
+    const result = getCoinCombination(25);
+
+    expect(result).toEqual([0, 0, 0, 1]);
+  });
+
+  it('1 dime, 1 nickel, 3 pennies', () => {
+    const result = getCoinCombination(18);
+
+    expect(result).toEqual([3, 1, 1, 0]);
+  });
+
+  it('2 quarter, 1 dime, 1 nickel, 2 pennies', () => {
+    const result = getCoinCombination(67);
+
+    expect(result).toEqual([2, 1, 1, 2]);
   });
 });
