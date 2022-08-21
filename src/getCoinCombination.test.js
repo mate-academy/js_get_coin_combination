b8f1b73e1f57239c851ea198c14a19c9c1bba2cd
Dmytro Borovskyi <borovskydmitry99@gmail.com>
2022-08-21 16:03:30 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
 'use strict';
+
+const { SearchSource } = require('jest');
 
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
@@ -7,4 +10,66 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return an array', () => {
+    expect(getCoinCombination())
+      .toBeInstanceOf(Array);
+  });
+
+  it(`return 0 when 'cents' is 0`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it('properly count boundary values', () => {
+    expect(getCoinCombination(2))
+      .toEqual([2, 0, 0, 0]);
+
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+
+    expect(getCoinCombination(9))
+      .toEqual([4, 1, 0, 0]);
+
+    expect(getCoinCombination(11))
+      .toEqual([1, 0, 1, 0]);
+
+    expect(getCoinCombination(24))
+      .toEqual([4, 0, 2, 0]);
+
+    expect(getCoinCombination(26))
+      .toEqual([1, 0, 0, 1]);
+  });
+
+  it('return all types of coins', () => {
+    expect(getCoinCombination(41))
+      .toEqual([1, 1, 1, 1]);
+  });
+
+  it('return combination for smallest possible number of coins', () => {
+    expect(getCoinCombination(82))
+      .toEqual([2, 1, 0, 3]);
+  });
+
+  it('return correct type for value given', () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
+  // it('throws on negative values', () => {
+  //   expect(getCoinCombination(-20))
+  //   .toTrow('Input positive values');
+  // });
 });
