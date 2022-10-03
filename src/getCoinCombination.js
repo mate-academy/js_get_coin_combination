2641b30cc3c0484eb85f05fb272a7b2d5597839a
sergii-nosachenko <s.nosachenko@gmail.com>
2022-10-03 17:50:25 +0300

    Added edge case to solution

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -10,6 +10,10 @@ function getCoinCombination(cents) {
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
 
+  if (cents <= 0) {
+    return coins;
+  }
+
   for (let i = 3; i >= 0; i--) {
     coins[i] = Math.floor(currentAmount / values[i]);
     currentAmount -= coins[i] * values[i];
