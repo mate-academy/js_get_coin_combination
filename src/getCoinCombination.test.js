0edff93ccf8a7d4494dfb5a5dcf5583f3eff8b10
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-11-30 12:52:23 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,29 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return zero without params`, () => {
+    expect(getCoinCombination())
+      .toEqual([ 0, 0, 0, 0 ]);
+  });
+
+  it(`should return 1 penny`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([ 1, 0, 0, 0 ]);
+  });
+
+  it(`should return 1 penny + 1 nickel`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([ 1, 1, 0, 0 ]);
+  });
+
+  it(`should return 2 pennies + 1 nickel + 1 dime`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([ 2, 1, 1, 0 ]);
+  });
+
+  it(`should return 2 quarters`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([ 0, 0, 0, 2 ]);
+  });
 });
