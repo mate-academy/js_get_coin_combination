ee96aafa36be33a2ac3f99fb9b3c51e67a4976d4
isid <r.bilokinskyi@gmail.com>
2022-12-25 12:01:51 +0200

    Test fixes

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,14 +8,44 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
+  test(`empty`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
   test(`1 penny`, () => {
     expect(getCoinCombination(1))
       .toEqual([1, 0, 0, 0]);
   });
 
+  test(`4 penny`, () => {
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+  });
+
+  test(`1 nickel`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
   test(`2 penny + 1 nickel`, () => {
-    expect(getCoinCombination(8))
-      .toEqual([3, 1, 0, 0]);
+    expect(getCoinCombination(7))
+      .toEqual([2, 1, 0, 0]);
+  });
+
+  test(`4 penny + 1 nickel`, () => {
+    expect(getCoinCombination(9))
+      .toEqual([4, 1, 0, 0]);
+  });
+
+  test(`1 dime`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  test(`1 dime + 1 nickel`, () => {
+    expect(getCoinCombination(15))
+      .toEqual([0, 1, 1, 0]);
   });
 
   test(`2 dime + 4 penny`, () => {
@@ -23,13 +53,33 @@ describe('getCoinCombination', () => {
       .toEqual([4, 0, 2, 0]);
   });
 
+  test(`1 quarter`, () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
   test(`1 quarter + 1 nickel + 2 penny`, () => {
     expect(getCoinCombination(32))
       .toEqual([2, 1, 0, 1]);
   });
 
+  test(`1 quarter + 1 dime +1 nickel + 1 penny`, () => {
+    expect(getCoinCombination(41))
+      .toEqual([1, 1, 1, 1]);
+  });
+
   test(`2 quarter + 1 dime + 1 nickel + 2 penny`, () => {
     expect(getCoinCombination(67))
       .toEqual([2, 1, 1, 2]);
+  });
+
+  test(`2 quarter + 2 dime + 4 penny`, () => {
+    expect(getCoinCombination(74))
+      .toEqual([4, 0, 2, 2]);
+  });
+
+  test(`3 quarter`, () => {
+    expect(getCoinCombination(75))
+      .toEqual([0, 0, 0, 3]);
   });
 });
