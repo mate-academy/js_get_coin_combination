2b19954ef18ab984d073e9bad25eebf7bd600444
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2022-12-26 14:04:23 +0200

    add more tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -18,19 +18,79 @@ describe('getCoinCombination', () => {
       .toEqual([2, 0, 0, 0]);
   });
 
+  it(`should return 4 penny`, () => {
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+  });
+
+  it(`should return 1 nikle`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
   it(`should return 1 nikle and 1 penny`, () => {
     expect(getCoinCombination(6))
       .toEqual([1, 1, 0, 0]);
   });
 
-  it(`should return 1 dime, 1 nikles and  4 penny`, () => {
+  it(`should return 1 nikle and 4 penny`, () => {
+    expect(getCoinCombination(9))
+      .toEqual([4, 1, 0, 0]);
+  });
+
+  it(`should return 1 dime`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return 1 dime, 1 nikle`, () => {
+    expect(getCoinCombination(15))
+      .toEqual([0, 1, 1, 0]);
+  });
+
+  it(`should return 1 dime, 1 nikle and 4 penny`, () => {
     expect(getCoinCombination(19))
       .toEqual([4, 1, 1, 0]);
   });
 
-  it(`should return 1 nickle 1 quarter`, () => {
+  it(`should return 2 dime and 4 penny`, () => {
+    expect(getCoinCombination(24))
+      .toEqual([4, 0, 2, 0]);
+  });
+
+  it(`should return 1 quarter`, () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return 1 nickle, 1 dime, 1 quarter`, () => {
+    expect(getCoinCombination(40))
+      .toEqual([0, 1, 1, 1]);
+  });
+
+  it(`should return 1 nickle, 1 quarter`, () => {
     expect(getCoinCombination(30))
       .toEqual([0, 1, 0, 1]);
+  });
+
+  it(`should return 1 penny, 1 nickle, 1 dime, 1 quarter`, () => {
+    expect(getCoinCombination(41))
+      .toEqual([1, 1, 1, 1]);
+  });
+
+  it(`should return 2 quarter`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should return 3 quarter`, () => {
+    expect(getCoinCombination(75))
+      .toEqual([0, 0, 0, 3]);
+  });
+
+  it(`should return 1 penny, 1 dime, 3 quarter`, () => {
+    expect(getCoinCombination(86))
+      .toEqual([1, 0, 1, 3]);
   });
 
   it(`should throw an error if number is negative`, () => {
