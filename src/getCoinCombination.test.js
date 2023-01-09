91fe12eefad92ad8f61351812433a8592f123510
Maryna Oznobishinaoznobishina.ok@gmail.comgit config --global user.name Maryna <oznobishina.ok@gmail.com>
2023-01-09 16:15:46 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,19 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test('Should return not falsy: ', () => {
+    expect(getCoinCombination(10)).not.toBeFalsy();
+  });
+
+  test('Should return defined: ', () => {
+    expect(getCoinCombination(7)).toBeDefined();
+  });
+
+  test('Should return corect equal: ', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
