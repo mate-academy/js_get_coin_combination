76c0ea307826c5996e5b278ed4c6897fa49659e1
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-13 20:59:52 +0100

    Fixed after review

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -15,42 +15,42 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Array);
   });
 
-  test('should return [0, 0, 0, 0] if arguments equel 0', () => {
+  test('should return [0, 0, 0, 0] if arguments equal 0', () => {
     const coins = getCoinCombination(0);
 
     expect(coins)
       .toEqual([0, 0, 0, 0]);
   });
 
-  test('should return [1, 0, 0, 0] if arguments equel 1', () => {
+  test('should return [1, 0, 0, 0] if arguments equal 1', () => {
     const coins = getCoinCombination(1);
 
     expect(coins)
       .toEqual([1, 0, 0, 0]);
   });
 
-  test('should return [0, 1, 0, 0] if arguments equel 5', () => {
+  test('should return [0, 1, 0, 0] if arguments equal 5', () => {
     const coins = getCoinCombination(5);
 
     expect(coins)
       .toEqual([0, 1, 0, 0]);
   });
 
-  test('should return [0, 0, 1, 0] if arguments equel 10', () => {
+  test('should return [0, 0, 1, 0] if arguments equal 10', () => {
     const coins = getCoinCombination(10);
 
     expect(coins)
       .toEqual([0, 0, 1, 0]);
   });
 
-  test('should return [0, 0, 0, 1] if arguments equel 50', () => {
+  test('should return [0, 0, 0, 1] if arguments equal 25', () => {
     const coins = getCoinCombination(25);
 
     expect(coins)
       .toEqual([0, 0, 0, 1]);
   });
 
-  test('should return [1, 0, 2, 2] if arguments equel 71', () => {
+  test('should return [1, 0, 2, 2] if arguments equal 71', () => {
     const coins = getCoinCombination(71);
 
     expect(coins)
