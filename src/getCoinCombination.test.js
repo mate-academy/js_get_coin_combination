a960e43d983a6a81587521839369908975b79319
Maryna Oznobishinaoznobishina.ok@gmail.comgit config --global user.name Maryna <oznobishina.ok@gmail.com>
2023-01-20 10:43:46 +0200

    solution 3

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -12,19 +12,19 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination(7)).toBeDefined();
   });
 
-  test('Should return correct equal: ', () => {
+  test('Should return correct result: ', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  test('Should return correct equal: ', () => {
+  test('Should return correct result: ', () => {
     expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  test('Should return correct equal: ', () => {
+  test('Should return correct result: ', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  test('Should return correct equal: ', () => {
+  test('Should return correct result: ', () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
