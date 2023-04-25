4990b59f34656ea53f07fa44083109c6e188e720
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2023-04-25 11:06:28 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,28 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return parts where coins[0] =
+   number of pennies(1 penny = 1 cent)`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return parts where coins[1] =
+   number of nickels (1 nickel = 5 cents)`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return parts where coins[2] =
+   number of dimes (1 dime = 10 cents)`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return parts where coins[3] =
+   number of quarters (1 quarter = 25 cents)`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
 });
