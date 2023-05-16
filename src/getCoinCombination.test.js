52d3f52cdee0ad1016ef5762e4638c0c499c79e7
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-16 16:20:07 +0300

    Changed

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,63 +8,112 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it('cents = 0', () => {
+  it(`should returt [0, 0, 0, 0] if 'cents' = 0`, () => {
     const actual = getCoinCombination(0);
     const expected = [0, 0, 0, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('0 < cents < 5', () => {
+  it(`should returt [1, 0, 0, 0] if 'cents' = 1`, () => {
+    const actual = getCoinCombination(1);
+    const expected = [1, 0, 0, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [3, 0, 0, 0] if 0 < 'cents' < 5`, () => {
     const actual = getCoinCombination(3);
     const expected = [3, 0, 0, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('cents = 5', () => {
+  it(`should returt [4, 0, 0, 0] if 'cents' = 4`, () => {
+    const actual = getCoinCombination(4);
+    const expected = [4, 0, 0, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [0, 1, 0, 0] if 'cents' = 5`, () => {
     const actual = getCoinCombination(5);
     const expected = [0, 1, 0, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('5 < cents < 10', () => {
+  it(`should returt [1, 1, 0, 0] if 'cents' = 6`, () => {
+    const actual = getCoinCombination(6);
+    const expected = [1, 1, 0, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [2, 1, 0, 0] if 5 < 'cents' < 10`, () => {
     const actual = getCoinCombination(7);
     const expected = [2, 1, 0, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('cents = 10', () => {
+  it(`should returt [4, 1, 0, 0] if 'cents' = 9`, () => {
+    const actual = getCoinCombination(9);
+    const expected = [4, 1, 0, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [0, 0, 1, 0] if 'cents' = 10`, () => {
     const actual = getCoinCombination(10);
     const expected = [0, 0, 1, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('10 < cents < 25', () => {
+  it(`should returt [1, 0, 1, 0] if 'cents' = 11`, () => {
+    const actual = getCoinCombination(11);
+    const expected = [1, 0, 1, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [0, 1, 1, 0] if 10 < 'cents' < 25`, () => {
     const actual = getCoinCombination(15);
     const expected = [0, 1, 1, 0];
 
     expect(actual).toEqual(expected);
   });
 
-  it('cents = 25', () => {
+  it(`should returt [1, 1, 2, 0] if 'cents' = 24`, () => {
+    const actual = getCoinCombination(24);
+    const expected = [4, 0, 2, 0];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [0, 0, 0, 1] if'cents' = 25`, () => {
     const actual = getCoinCombination(25);
     const expected = [0, 0, 0, 1];
 
     expect(actual).toEqual(expected);
   });
 
-  it('cents > 25', () => {
+  it(`should returt [1, 0, 0, 1] if'cents' = 26`, () => {
+    const actual = getCoinCombination(26);
+    const expected = [1, 0, 0, 1];
+
+    expect(actual).toEqual(expected);
+  });
+
+  it(`should returt [2, 1, 1, 2] if 'cents' = 67`, () => {
     const actual = getCoinCombination(67);
     const expected = [2, 1, 1, 2];
 
     expect(actual).toEqual(expected);
   });
 
-  it('cents is NaN', () => {
+  it(`should returt [0, 0, 0, 0] if'cents' is NaN`, () => {
     const actual = getCoinCombination(NaN);
     const expected = [NaN, NaN, NaN, NaN];
 
