2f7cc7e9567d669dab815abd1ce096c8be86e8ba
Yurii Demkiv <demkiv123@gmail.com>
2023-05-17 15:12:13 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,34 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should throw error when input is lower than 0`, () => {
+    expect(getCoinCombination(-1))
+      .toThrow();
+  });
+
+  it(`should return array with zeros when input equals 0`, () => {
+    expect(getCoinCombination(0))
+      .toStrictEqual([0, 0, 0, 0]);
+  });
+
+  it(`should properly count pennies`, () => {
+    expect(getCoinCombination(1))
+      .toStrictEqual([1, 0, 0, 0]);
+  });
+
+  it(`should properly count nickels`, () => {
+    expect(getCoinCombination(6))
+      .toStrictEqual([1, 1, 0, 0]);
+  });
+
+  it(`should properly count dimes`, () => {
+    expect(getCoinCombination(17))
+      .toStrictEqual([2, 1, 1, 0]);
+  });
+
+  it(`should properly count quarters`, () => {
+    expect(getCoinCombination(50))
+      .toStrictEqual([0, 0, 0, 2]);
+  });
 });
