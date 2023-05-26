314d608339c371cb5e19196606c180793f0f5eaa
svitjojo <kornotavr@gmail.com>
2023-05-26 15:59:48 +0300

    test implemented

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,28 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should count pennies', () => {
+    const expected = getCoinCombination(1);
+
+    expect(expected).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should count nickels', () => {
+    const expected = getCoinCombination(6);
+
+    expect(expected).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should count dimes', () => {
+    const expected = getCoinCombination(17);
+
+    expect(expected).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should count quarters', () => {
+    const expected = getCoinCombination(50);
+
+    expect(expected).toEqual([0, 0, 0, 2]);
+  });
 });
