d163b91b793ed1bff331c6991d3c7a042cb8f0a8
Natasha <kava221998@gmail.com>
2023-06-07 16:22:31 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return number of pennies`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return number of nickels`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return number of dimes`, () => {
+    expect(getCoinCombination(2))
+      .toEqual([2, 0, 0, 0]);
+  });
+
+  it(`should return number of quarters`, () => {
+    expect(getCoinCombination(3))
+      .toEqual([3, 0, 0, 0]);
+  });
 });
