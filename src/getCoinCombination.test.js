929f40d64f25121c9c904aac0f58158baf6b94f6
Ruslan Vrazovskyi <dnkons@gmail.com>
2023-06-20 23:49:41 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,14 +3,35 @@
 const { getCoinCombination } = require('./getCoinCombination');
 
 describe('getCoinCombination', () => {
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
 
-  it('should convert cents to coin combination correctly', () => {
+  it('should return [0, 0, 0, 0] when cents is 0', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return [1, 0, 0, 0] when cents is 1', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
-    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return [0, 1, 0, 0] when cents is 5', () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it('should return [0, 0, 1, 0] when cents is 10', () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it('should return [0, 0, 0, 1] when cents is 25', () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it('should return [2, 1, 1, 0] when cents is 17', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return [0, 0, 0, 2] when cents is 50', () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
