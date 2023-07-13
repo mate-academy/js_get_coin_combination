6dd17f2252a96b58dc89c6afc3e1cb0223af64da
Natasha <kava221998@gmail.com>
2023-07-13 12:27:07 +0300

    Fix tests & descriptions

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,23 +8,24 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`should return number of pennies`, () => {
-    expect(getCoinCombination(0))
-      .toEqual([0, 0, 0, 0]);
-  });
-
-  it(`should return number of nickels`, () => {
+ 
+  it(`should return correct array of pennies for 1 cent`, () => {
     expect(getCoinCombination(1))
       .toEqual([1, 0, 0, 0]);
   });
 
-  it(`should return number of dimes`, () => {
-    expect(getCoinCombination(2))
-      .toEqual([2, 0, 0, 0]);
+  it(`should return correct array of nickels for 6 cents`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
   });
 
-  it(`should return number of quarters`, () => {
-    expect(getCoinCombination(3))
-      .toEqual([3, 0, 0, 0]);
+  it(`should return correct array of dimes for 17 cents`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return correct array of quarters for 50 cents`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
   });
 });
