1d82876211619946c8c9ac55f4865185d42271b2
Myroslava Levchenko <mira.rudd@gmail.com>
2023-08-10 22:40:19 +0300

    Revert "Result"
    This reverts commit 2254667c369e8cbcd6b66aeb2b7039e4529cb82f.

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,20 +7,4 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
-
-  test('coins are equal 1', () => {
-    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
-  });
-
-  test('coins are eqqual 6', () => {
-    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
-  });
-
-  test('coins are equal 17', () => {
-    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
-  });
-
-  test('coins are equal 50', () => {
-    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
-  });
 });
