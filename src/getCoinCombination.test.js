7fe5a95d9cec4f1bebb3f8feb20a113fffc8663a
Oleksandr Kapustei <angelion541@gmail.com>
2023-08-16 15:59:40 +0300

    finish tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,47 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return [0, 0, 0, 0] for 0', () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return correct pennies for 0 < value < 5', () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+  });
+
+  it('should return correct nickels for 4 < value < 9', () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+
+    expect(getCoinCombination(9))
+      .toEqual([4, 1, 0, 0]);
+  });
+
+  it('should return the correct dimes for 8 < value < 25', () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+
+    expect(getCoinCombination(15))
+      .toEqual([0, 1, 1, 0]);
+  });
+
+  it('return the correct combination of quarters for 24 < value', () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+
+    expect(getCoinCombination(67))
+      .toEqual([2, 1, 1, 2]);
+  });
 });
