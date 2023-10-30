e9dedb0af2443c9e397ee4ac73fddb1b9803f637
Barbara Krysiak <b.krysiakk@gmail.com>
2023-10-30 20:24:53 +0100

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,6 +1,8 @@
 'use strict';
+
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
+
   it(`should be declared`, () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
@@ -8,21 +10,21 @@ describe('getCoinCombination', () => {
 
   it(`should return [1, 0, 0, 0]`
   + ` if argument === 1`, () => {
-    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0])
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
   it(`should return [1, 1, 0, 0]`
   + ` if argument === 6`, () => {
-    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0])
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
   it(`should return [2, 1, 1, 0]`
   + ` if argument === 17`, () => {
-    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0])
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
   it(`should return [0, 0, 0, 2]`
   + ` if argument === 50`, () => {
-    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2])
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
