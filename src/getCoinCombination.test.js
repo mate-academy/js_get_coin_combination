909ff41c759b9d59ef44667166d2446c2f53b175
sheva-laptop <vladislavshevchenko10@gmail.com>
2023-11-16 19:48:43 +0200

    add solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,22 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it('should return 1 penny for 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return 1 nickel for 5 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return 1 dime for 10 cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return 1 quarter for 25 cents', () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
   });
 });
