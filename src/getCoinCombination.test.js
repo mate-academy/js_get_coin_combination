0a4771519d9dad6deb00d602ebc3add214bfcc38
Julia14012000 <Juliamilan1414@gmail.com>
2024-02-05 20:06:53 +0200

    Improve titles

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,39 +8,40 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`arg number - add penny`, () => {
-    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
-  });
+  it('should correctly handle adding pennies', () => {
+  expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+});
 
-  it(`arg number - add nickel`, () => {
-    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
-  });
+it('should correctly handle adding nickels', () => {
+  expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+});
 
-  it(`arg number - add dime`, () => {
-    expect(getCoinCombination(75)).toEqual([0, 0, 0, 3]);
-  });
+it('should correctly handle adding dimes', () => {
+  expect(getCoinCombination(75)).toEqual([0, 0, 0, 3]);
+});
 
-  it(`arg number - add quarters`, () => {
-    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
-  });
+it('should correctly handle adding quarters', () => {
+  expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+});
 
-  it(`arg number - add penny and nickel`, () => {
-    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
-  });
+it('should correctly handle adding pennies and nickels', () => {
+  expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+});
 
-  it(`arg number - add dime and quarters`, () => {
-    expect(getCoinCombination(70)).toEqual([0, 0, 2, 2]);
-  });
+it('should correctly handle adding dimes and quarters', () => {
+  expect(getCoinCombination(70)).toEqual([0, 0, 2, 2]);
+});
 
-  it(`arg fractional number`, () => {
-    expect(getCoinCombination(1.5)).toEqual([1, 0, 0, 0]);
-  });
+it('should correctly handle fractional numbers by adding pennies', () => {
+  expect(getCoinCombination(1.5)).toEqual([1, 0, 0, 0]);
+});
 
-  it(`arg invalid value`, () => {
-    expect(getCoinCombination('test')).toEqual([NaN, NaN, NaN, NaN]);
-  });
+it('should correctly handle invalid values by returning NaN for all coin types', () => {
+  expect(getCoinCombination('test')).toEqual([NaN, NaN, NaN, NaN]);
+});
 
-  it(`arg negative number`, () => {
-    expect(getCoinCombination(-100)).toEqual([0, 0, 0, -4]);
-  });
+it('should correctly handle negative numbers by adding negative quarters', () => {
+  expect(getCoinCombination(-100)).toEqual([0, 0, 0, -4]);
+});
+
 });
