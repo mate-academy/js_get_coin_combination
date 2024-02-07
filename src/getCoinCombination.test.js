abf2bc9e22d32a48cf348e9916de010be68bb307
Alina <aliska.nofear@gmail.com>
2024-02-07 14:26:41 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,59 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('combination of coins for 1 cent', () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it('combination of coins for cent > 5', () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it('combination of coins for cent > 15', () => {
+    const result = getCoinCombination(16);
+
+    expect(result).toEqual([1, 1, 1, 0]);
+  });
+
+  it('combination of coins for cent >= 50', () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+  });
+
+  it('combination of coins for zero cents', () => {
+    const result = getCoinCombination(0);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it('combination of coins for NaN value cents', () => {
+    const result = getCoinCombination('a');
+
+    expect(result).toEqual([NaN, NaN, NaN, NaN]);
+  });
+
+  it('combination of coins for number with decimal part', () => {
+    const result = getCoinCombination(1.5);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it('combination of coins if number of coins are omitted', () => {
+    const result = getCoinCombination();
+
+    expect(result).toEqual([NaN, NaN, NaN, NaN]);
+  });
+
+  it('combination of coins if extra coin is added', () => {
+    const result = getCoinCombination(1, 6);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
 });
