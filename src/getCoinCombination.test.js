c7f115b193c707a5eb8ea169d3c0ca3500a9c82e
Tetiana Maksiuk <TetianaMaksiuk@gmail.com>
2024-02-07 21:08:04 +0200

    Add fixed messages

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,51 +8,51 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it('should return correct coin combination for 0 cents', () => {
+  it('should return an array [0, 0, 0, 0] for argument with 0 value', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  it('should return correct coin combination for 1 cent', () => {
+  it('should return an array [1, 0, 0, 0] for argument with 1 value', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it('should return correct coin combination for 2 cents', () => {
+  it('should return an array [2, 0, 0, 0] for argument with 2 value', () => {
     expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
   });
 
-  it('should return correct coin combination for 4 cents', () => {
+  it('should return an array [4, 0, 0, 0] for argument with 4 value', () => {
     expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
   });
 
-  it('should return correct coin combination for 5 cents', () => {
+  it('should return an array [0, 1, 0, 0] for argument with 5 value', () => {
     expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
   });
 
-  it('should return correct coin combination for 6 cents', () => {
+  it('should return an array [1, 1, 0, 0] for argument with 6 value', () => {
     expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it('should return correct coin combination for 9 cents', () => {
+  it('should return an array [4, 1, 0, 0] for argument with 9 value', () => {
     expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
   });
 
-  it('should return correct coin combination for 10 cents', () => {
+  it('should return an array [0, 0, 1, 0] for argument with 10 value', () => {
     expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
   });
 
-  it('should return correct coin combination for 11 cents', () => {
+  it('should return an array [1, 0, 1, 0] for argument with 11 value', () => {
     expect(getCoinCombination(11)).toEqual([1, 0, 1, 0]);
   });
 
-  it('should return correct coin combination for 24 cents', () => {
+  it('should return an array [4, 0, 2, 0] for argument with 24 value', () => {
     expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
   });
 
-  it('should return correct coin combination for 25 cents', () => {
+  it('should return an array [0, 0, 0, 1] for argument with 25 value', () => {
     expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
   });
 
-  it('should return correct coin combination for 26 cents', () => {
+  it('should return an array [1, 0, 0, 1] for argument with 26 value', () => {
     expect(getCoinCombination(26)).toEqual([1, 0, 0, 1]);
   });
 });
