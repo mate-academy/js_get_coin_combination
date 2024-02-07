10e88c3d3a431b14a64fb8fd5d245cdfacfae77a
Tetiana Maksiuk <TetianaMaksiuk@gmail.com>
2024-02-07 16:44:01 +0200

    add tests for getCoinCombination

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,52 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return correct coin combination for 0 cents', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return correct coin combination for 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return correct coin combination for 2 cents', () => {
+    expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
+  });
+
+  it('should return correct coin combination for 4 cents', () => {
+    expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
+  });
+
+  it('should return correct coin combination for 5 cents', () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it('should return correct coin combination for 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return correct coin combination for 9 cents', () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+  });
+
+  it('should return correct coin combination for 10 cents', () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it('should return correct coin combination for 11 cents', () => {
+    expect(getCoinCombination(11)).toEqual([1, 0, 1, 0]);
+  });
+
+  it('should return correct coin combination for 24 cents', () => {
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
+  });
+
+  it('should return correct coin combination for 25 cents', () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it('should return correct coin combination for 26 cents', () => {
+    expect(getCoinCombination(26)).toEqual([1, 0, 0, 1]);
+  });
 });
