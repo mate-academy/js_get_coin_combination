f47a2392507e73644d63a56c8cda405db149df15
Oksana Kachmar <profiscribe@gmail.com>
2024-02-09 20:49:01 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,19 +8,35 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it('should return combination for 1 cent', () => {
+  it('should return [1, 0, 0, 0] for argument 1', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it('should return combination for 6 cents', () => {
+  it('should return [1, 1, 0, 0] for argument 6', () => {
     expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it('should return combination for 17 cents', () => {
+  it('should return [2, 1, 1, 0] for argument 17', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it('should return combination for 50 cents', () => {
+  it('should return [0, 0, 0, 2] for argument 50', () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it('should return [0, 0, 1, 0] for argument 10', () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it('should return [1, 0, 1, 0] for argument 11', () => {
+    expect(getCoinCombination(11)).toEqual([1, 0, 1, 0]);
+  });
+
+  it('should return [1, 1, 1, 1] for argument 41', () => {
+    expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
+  });
+
+  it('should return [3, 0, 0, 4] for argument 103', () => {
+    expect(getCoinCombination(103)).toEqual([3, 0, 0, 4]);
   });
 });
