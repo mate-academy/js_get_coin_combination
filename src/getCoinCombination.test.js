fa9847db15319877eb15d621fa6bc4ed20149aa7
Sandra <malinevskaqa@gmail.com>
2024-04-03 19:06:44 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,88 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(7))
+      .toEqual([2, 1, 0, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(16))
+      .toEqual([1, 1, 1, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(18))
+      .toEqual([3, 1, 1, 0]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(49))
+      .toEqual([4, 0, 2, 1]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
+});
+
+describe('getCoinCombination', () => {
+  const { getCoinCombination } = require('./getCoinCombination');
+
+  it(`should be declared`, () => {
+    expect(getCoinCombination(51))
+      .toEqual([1, 0, 0, 2]);
   });
 });
