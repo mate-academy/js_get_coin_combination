23c7bcc9d422ec9bf84c9461a1a7685a261cf935
Oleksii <aleksey.gudilkin@gmail.com>
2024-04-03 16:32:40 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,28 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return [1, 0, 0, 0] when receives 1 cents', () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return [1, 1, 0, 0] when receives 6 cents', () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return [2, 1, 1, 0] when receives 17 cents', () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return [0, 0, 0, 2] when receives 50 cents', () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+  });
 });
