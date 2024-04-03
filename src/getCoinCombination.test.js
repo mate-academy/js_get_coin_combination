592f1d136bc812e7d91f931f7de715951a0d4b0c
kvoitsekhovska <voitsekhovskakateryna@gmail.com>
2024-04-03 20:43:17 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,8 +3,33 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+  it('should return an array of length 4', () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toHaveLength(4);
+  });
+
+  it('should return the correct combination for 1 cent', () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return the correct combination for 6 cents', () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return the correct combination for 17 cents', () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return the correct combination for 50 cents', () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
   });
 });
