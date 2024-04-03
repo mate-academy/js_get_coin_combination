c7b7c60a4f2bba69cd4b0ba9de0efdab234a6b49
Kateryna Nedobytko <knedobytko@gmail.com>
2024-04-04 00:01:56 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test('First element is my cats names in human years', () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  test('First element is my cats names in human years', () => {
+    expect(getCoinCombination(15))
+      .toEqual([0, 1, 1, 0]);
+  });
+
+  test('First element is my cats names in human years', () => {
+    expect(getCoinCombination(30))
+      .toEqual([0, 1, 0, 1]);
+  });
+
+  test('First element is my cats names in human years', () => {
+    expect(getCoinCombination(75))
+      .toEqual([0, 0, 0, 3]);
+  });
 });
