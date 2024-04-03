7dd9c9bbd1b5cf61f9be11078936c4be5513a94b
KurchakKhristina <kurchakkristina6@gmail.com>
2024-04-03 15:05:49 +0300

    added tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,8 +3,27 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+  test('should be declared', () => {
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0, 0, 0] for 0 cents', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  test('should return [1, 0, 0, 0] for 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  test('should return [1, 1, 0, 0] for 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  test('should return [2, 1, 1, 0] for 17 cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  test('should return [0, 0, 0, 2] for 50 cents', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
