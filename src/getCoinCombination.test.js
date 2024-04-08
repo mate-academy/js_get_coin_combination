89c21909918eee0aced62e97a38c413dcf4c8192
kvoitsekhovska <voitsekhovskakateryna@gmail.com>
2024-04-08 19:52:24 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,33 +3,24 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it('should return an array of length 4', () => {
+  it('should return an array of length 4 for 1 cent', () => {
     const result = getCoinCombination(1);
-
     expect(result).toHaveLength(4);
   });
 
-  it('should return the correct combination for 1 cent', () => {
-    const result = getCoinCombination(1);
-
-    expect(result).toEqual([1, 0, 0, 0]);
+  it("should return '1' pennie when '1' is entered", () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it('should return the correct combination for 6 cents', () => {
-    const result = getCoinCombination(6);
-
-    expect(result).toEqual([1, 1, 0, 0]);
+  it("should return '1' pennie and '1' nickel when '6' is entered", () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it('should return the correct combination for 17 cents', () => {
-    const result = getCoinCombination(17);
-
-    expect(result).toEqual([2, 1, 1, 0]);
+  it("should return '2' pennies, '1' nickel, and '1' dime when '17' is entered", () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it('should return the correct combination for 50 cents', () => {
-    const result = getCoinCombination(50);
-
-    expect(result).toEqual([0, 0, 0, 2]);
+  it("should return '2' quarters when '50' is entered", () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
