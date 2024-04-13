413d0141869b748e200c36d80a6c1f1971c0f82a
Kateryna Nedobytko <knedobytko@gmail.com>
2024-04-13 21:04:01 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,22 +8,22 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return a dime, when cents is equal to 10', () => {
+  test('should return [0, 0, 1, 0] for 10 cents', () => {
     expect(getCoinCombination(10))
       .toEqual([0, 0, 1, 0]);
   });
 
-  test('should return a nickel and a dime, when cents is equal to 15', () => {
+  test('should return [0, 1, 1, 0] for 15 cents', () => {
     expect(getCoinCombination(15))
       .toEqual([0, 1, 1, 0]);
   });
 
-  test('should return 1 nickel, 1 quarter, when cents is equal to 30', () => {
+  test('should return [0, 1, 0, 1] for 30 cents', () => {
     expect(getCoinCombination(30))
       .toEqual([0, 1, 0, 1]);
   });
 
-  test('should return 3 quarters, when cents is equal to 75', () => {
+  test('should return [0, 0, 0, 3] for 75 cents', () => {
     expect(getCoinCombination(75))
       .toEqual([0, 0, 0, 3]);
   });
