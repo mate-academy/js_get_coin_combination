9fd57648be2f9e6e40dff33555fbb7bb5c5be937
TykhonKozachenko <tooliewae@gmail.com>
2024-04-14 09:48:52 +0200

    tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,8 +3,33 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
+  it(`function should be declared`, () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
+  });
+
+  it('should return correct coin combination for 0 cents', () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return correct coin combination for 1 cent`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return correct coin combination for 6 cents`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return correct coin combination for 17 cents`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return correct coin combination for 50 cents`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
   });
 });
