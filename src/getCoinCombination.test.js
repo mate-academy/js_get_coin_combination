45e3b3d84d7101e8db17ca74c666661c1d437dad
TykhonKozachenko <tooliewae@gmail.com>
2024-04-14 18:40:05 +0200

    tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -18,9 +18,29 @@ describe('getCoinCombination', () => {
       .toEqual([1, 0, 0, 0]);
   });
 
+  it(`should return correct coin combination for 3 cents`, () => {
+    expect(getCoinCombination(3))
+      .toEqual([3, 0, 0, 0]);
+  });
+
+  it(`should return correct coin combination for 5 cents`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
   it(`should return correct coin combination for 6 cents`, () => {
     expect(getCoinCombination(6))
       .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return correct coin combination for 10 cents`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return correct coin combination for 15 cents`, () => {
+    expect(getCoinCombination(15))
+      .toEqual([0, 1, 1, 0]);
   });
 
   it(`should return correct coin combination for 17 cents`, () => {
@@ -31,5 +51,10 @@ describe('getCoinCombination', () => {
   it(`should return correct coin combination for 50 cents`, () => {
     expect(getCoinCombination(50))
       .toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should return correct coin combination for 69 cents`, () => {
+    expect(getCoinCombination(69))
+      .toEqual([4, 1, 1, 2]);
   });
 });
