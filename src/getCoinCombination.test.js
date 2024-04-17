35c3890ac1db0414fe327e2259b30a5e6bd2f4d4
Nivon35 <Nivon35@gmail.com>
2024-04-17 16:29:28 +0200

    Implementation of tests for getCoinCombination function

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return [4, 0, 0, 0] for 4 cents', () => {
+    expect(getCoinCombination(4))
+      .toEqual([4, 0, 0, 0]);
+  });
+
+  it('should return [0, 0, 0, 0] for 0 cents', () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return [0, 0, 0, 0] for 0.5 cents', () => {
+    expect(getCoinCombination(0.5))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return [4, 0, 1, 5] for 139 cents', () => {
+    expect(getCoinCombination(139))
+      .toEqual([4, 0, 1, 5]);
+  });
 });
