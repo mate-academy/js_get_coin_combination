b2e77ee8a0c56980a48776d4ddb381376f4bb0de
Maria <maria.szynaka@gmail.com>
2024-04-18 09:29:33 +0200

    Solution_fixed_variable_added

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,22 +9,26 @@ describe('getCoinCombination', () => {
   });
 
   test(`should return 1 penny when input 1 cent`, () => {
-    expect(getCoinCombination(1))
+    const inputCent = 1;
+    expect(getCoinCombination(inputCent))
       .toEqual([1, 0, 0, 0]);
   });
 
   test(`should return 1 penny + 1 nickel when input 6 cent`, () => {
-    expect(getCoinCombination(6))
+    const inputCent = 6;
+    expect(getCoinCombination(inputCent))
       .toEqual([1, 1, 0, 0]);
   });
 
   test(`should return 2 pennies + 1 nickel + 1 dime when input 17 cent`, () => {
-    expect(getCoinCombination(17))
+    const inputCent = 17;
+    expect(getCoinCombination(inputCent))
       .toEqual([2, 1, 1, 0]);
   });
 
   test(`should return 2 quarters when input 50 cent`, () => {
-    expect(getCoinCombination(50))
+    const inputCent = 50;
+    expect(getCoinCombination(inputCent))
       .toEqual([0, 0, 0, 2]);
   });
 
