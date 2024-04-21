cca3d74920478204675fbfb630a0579a417e1080
Sandra <malinevskaqa@gmail.com>
2024-04-21 21:58:11 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,89 +3,43 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination(0))
-      .toEqual([0, 0, 0, 0]);
+  it('should be declared', () => {
+    expect(getCoinCombination).toBeInstanceOf(Function);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(5))
-      .toEqual([0, 1, 0, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(6))
-      .toEqual([1, 1, 0, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(7))
-      .toEqual([2, 1, 0, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(16))
-      .toEqual([1, 1, 1, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(16)).toEqual([1, 1, 1, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(17))
-      .toEqual([2, 1, 1, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(18))
-      .toEqual([3, 1, 1, 0]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(18)).toEqual([3, 1, 1, 0]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(49))
-      .toEqual([4, 0, 2, 1]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(49)).toEqual([4, 0, 2, 1]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(50))
-      .toEqual([0, 0, 0, 2]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
-});
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
-  it(`should be declared`, () => {
-    expect(getCoinCombination(51))
-      .toEqual([1, 0, 0, 2]);
+  test('should return correct coin combination', () => {
+    expect(getCoinCombination(51)).toEqual([1, 0, 0, 2]);
   });
 });
