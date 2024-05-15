2e39721bb5f553776564ad620349ba296c2e551a
Serhii Vasyliev <vv44980@gmail.com>
2024-05-14 22:02:13 -0600

    title is fixed

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,33 +8,31 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
   
-  test('should return 0`s if original data is 0', () => {
+  test('should return 0`s, if the user inputs 0 cents', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0])
    });
 
-   test('should return [0, 0, 0, 4] if original data is 100', () => {
+   test('should return 4 quarter, if the user inputs 100 cents', () => {
     expect(getCoinCombination(100)).toEqual([0, 0, 0, 4])
    });
 
-   test('should return [1, 1, 1, 1] if original data is 41', () => {
+   test('should return the least amount of coins, if the user inputs 41 cents', () => {
     expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
    });
 
-   test('should return [0, 1, 1, 1] if original data is 40', () => {
+   test('should return the least amount of coins, if the user inputs 40 cents', () => {
     expect(getCoinCombination(40)).toEqual([0, 1, 1, 1]);
    });
 
-   test('should return [4, 0, 0, 0] if original data is 4', () => {
+   test('should return the least amount of coins, if the user inputs 4 cents', () => {
     expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
    });
 
-   test('should return [0, 1, 0, 1] if original data is 30', () => {
+   test('should return the least amount of coins, if the user inputs 30 cents', () => {
     expect(getCoinCombination(30)).toEqual([0, 1, 0, 1]);
    });
 
-   test('should return [1, 1, 0, 40] if original data is 1006', () => {
+   test('should return the least amount of coins, if the user inputs 1006 cents', () => {
     expect(getCoinCombination(1006)).toEqual([1, 1, 0, 40]);
    });
 });
-
-   
