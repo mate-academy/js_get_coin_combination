8eccbb39f20eaf86ab6da293e02aa3c780a60d6e
MishaKluch <nimamiha@gmail.com>
2024-06-21 22:44:00 +0300

    Сorrection of headers

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -12,27 +12,27 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  test('should return correct combination of coins for 5 cents', () => {
+  test('should return 1 nickel when 5 cents is entered' , () => {
     expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
   });
 
-  test('should return correct combination of coins for 7 cents', () => {
+  test('should return 1 nickel and 2 pennies when 7 cents is entered', () => {
     expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
   });
 
-  test('should return correct combination of coins for 10 cents', () => {
+  test('should return 1 dime when 10 cents is entered', () => {
     expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
   });
 
-  test('should return correct combination of coins for 17 cents', () => {
+  test('should return 2 pennies, 1 nickel and 1 dime when 17cents is entered', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  test('should return correct combination of coins for 25 cents', () => {
+  test('should return 1 quarter when 25 cents is entered', () => {
     expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
   });
 
-  test('should return correct combination of coins for 97 cents', () => {
+  test('should return 2pennies, 2 dimes, 3 quarters when 97 cents is entered', () => {
     expect(getCoinCombination(97)).toEqual([2, 0, 2, 3]);
   });
 });
