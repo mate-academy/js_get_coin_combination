0bd041356436b174b7b04179a38d15aec363bd86
Khrystyna Kazva <kristinakazva@gmail.com>
2024-06-21 19:03:02 +0300

    Add tests for getCoinCombination function

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test(`should return 1 penny for 1 cent`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  test(`should return 1 penny and 1 nickel for 6 cents`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  test(`should return 2 pennies, 1 nickel and 1 dime for 17 cents`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  test(`should return 2 quarters for 50 cents`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
 });
