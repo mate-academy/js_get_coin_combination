e7e1e2cd5818d455e80d5ccf9649e84ea1e8d733
yurayagu <yurayagu@gmail.com>
2024-06-21 20:31:41 +0300

    Coin Combination

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('for 1 cent [1, 0, 0, 0]', () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it('for 6 cents [1, 1, 0, 0]', () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it('for 17 cents [2, 1, 1, 0]', () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it('for 50 cents [0, 0, 0, 2]', () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
+  });
 });
