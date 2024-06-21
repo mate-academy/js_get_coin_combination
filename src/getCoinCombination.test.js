3b5e42c12f4c6cc8b5e6695ed0f21382c54bad61
Anna Hutnichenko <gutnichenkoo@gmail.com>
2024-06-21 17:44:50 +0300

    #2 fixed solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -14,32 +14,34 @@ describe('getCoinCombination', () => {
     expect(result.length).toBe(4);
   });
 
-  it(`should return 0 coin for 0 cent`, () => {
+  it(`should return 0 coin when '0' is entered`, () => {
     expect(getCoinCombination(0))
       .toStrictEqual([0, 0, 0, 0]);
   });
 
-  it(`should return 1 penny for 1 cent`, () => {
+  it(`should return 1 penny when '1' is entered`, () => {
     expect(getCoinCombination(1))
       .toStrictEqual([1, 0, 0, 0]);
   });
 
-  it(`should return 1 penny and 1 nickel for 6 cent`, () => {
+  it(`should return 1 penny and 1 nickel when '6' is entered`, () => {
     expect(getCoinCombination(6))
       .toStrictEqual([1, 1, 0, 0]);
   });
 
-  it(`should return 2 pennies, 1 nickel and 1 dime for 17 cent`, () => {
+  it(`should return 2 pennies, 1 nickel and 1 dime
+    when '17' is entered`, () => {
     expect(getCoinCombination(17))
       .toStrictEqual([2, 1, 1, 0]);
   });
 
-  it(`should 2 quarters for 50 cent`, () => {
+  it(`should 2 quarters when '50 is entered`, () => {
     expect(getCoinCombination(50))
       .toStrictEqual([0, 0, 0, 2]);
   });
 
-  it(`should return 4 pennies, 2 dimes and 39 quarters for 999 cent`, () => {
+  it(`should return 4 pennies, 2 dimes and 39 quarters
+    when '999' is entered`, () => {
     expect(getCoinCombination(999))
       .toStrictEqual([4, 0, 2, 39]);
   });
