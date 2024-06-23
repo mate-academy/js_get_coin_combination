690df5a37c11d5007efae53559696abc28028d30
Oleh Bokufai <bokufaioleh@gmail.com>
2024-06-23 15:24:05 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return '1' penny when '1' is entered`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return '1' nickel when '5' is entered`, () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return '1' dime when '10' is entered`, () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return '1' quarters when '25' is entered`, () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
 });
