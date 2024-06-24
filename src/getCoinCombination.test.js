284cb7331ae8b766950a7919ae3e2b968dc064ed
InnaKurtieva <nezaleznaua2308@ukr.net>
2024-06-24 20:24:58 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,18 +9,18 @@ describe('getCoinCombination', () => {
   });
 
   it('should return coins if a values([1, 1, 0, 0])', () => {
-    expect(getCoinCombination(6));
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
   it('should return coins if a values([2, 1, 1, 0])', () => {
-    expect(getCoinCombination(17));
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
   it('should return coins if a values([0, 0, 0, 2])', () => {
-    expect(getCoinCombination(50));
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 
   it('should return coins if a values([1, 0, 0, 0])', () => {
-    expect(getCoinCombination(1));
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 });
