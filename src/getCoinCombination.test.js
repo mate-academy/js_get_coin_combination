e30cfc225cb327b3728ae69cb86674978964960c
Olha Panchenko <panchenko-85@ukr.net>
2024-06-28 19:33:41 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('getCoinCombination', () => {
@@ -12,6 +13,18 @@ describe('getCoinCombination', () => {
   it('should handle edge cases', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should handle boundary values', () => {
+    expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+    expect(getCoinCombination(11)).toEqual([1, 0, 1, 0]);
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+    expect(getCoinCombination(26)).toEqual([1, 0, 0, 1]);
   });
 
   it('should return an array with four elements', () => {
