3d66759705e9f05ab81ed0cb72de7803539b671b
retur <return0fthekingv@gmail.com>
2024-06-29 14:43:36 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,37 +8,38 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`should return an array with zeros`, () => {
+  it(`should return [0, 0, 0, 0] for 0 cents`, () => {
     expect(getCoinCombination(0))
       .toEqual([0, 0, 0, 0]);
   });
 
-  it(`should return an array with when we have each one type of coins `, () => {
+  // eslint-disable-next-line max-len
+  it(`should return the correct combination when we have each type of coins `, () => {
     expect(getCoinCombination(41))
       .toEqual([1, 1, 1, 1]);
   });
 
-  it(`should return an array when we have 1 penny`, () => {
+  it(`should return the correct combination when we have 1 penny`, () => {
     expect(getCoinCombination(1))
       .toEqual([1, 0, 0, 0]);
   });
 
-  it(`should return an array when we have 1 nickel`, () => {
+  it(`should return the correct combination when we have 1 nickel`, () => {
     expect(getCoinCombination(5))
       .toEqual([0, 1, 0, 0]);
   });
 
-  it(`should return an array when we have 1 dime`, () => {
+  it(`should return the correct combination when we have 1 dime`, () => {
     expect(getCoinCombination(10))
       .toEqual([0, 0, 1, 0]);
   });
 
-  it(`should return an array when we have 1 quarter`, () => {
+  it(`should return the correct combination when we have 1 quarter`, () => {
     expect(getCoinCombination(25))
       .toEqual([0, 0, 0, 1]);
   });
 
-  it(`should return an array when cents = 99`, () => {
+  it(`should return the correct combination when cents = 99`, () => {
     expect(getCoinCombination(99))
       .toEqual([4, 0, 2, 3]);
   });
