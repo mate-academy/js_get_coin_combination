97f9e2392a5e119502472229f4c54d20884f8b95
DDefty <dawid.kogut99@gmail.com>
2024-07-04 12:36:04 +0200

    Tests added

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,32 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+  test(`returns correct combination for 1 cent`, () => {
+      expect(getCoinCombination(1))
+      .toEqual([1,0,0,0])
+  });
+  test(`returns correct combination for 6 cent`, () => {
+      expect(splitInteger(6))
+      .toEqual([1,1,0,0])
+  });
+  test(`returns correct combination for 17 cent`, () => {
+      expect(splitInteger(17))
+      .toEqual([2,1,1,0])
+  });
+  test(`returns correct combination for 50 cent`, () => {
+      expect(splitInteger(50))
+      .toEqual([0,0,0,2])
+  });
+  test(`returns correct combination for 0 cent`, () => {
+    expect(splitInteger(0))
+    .toEqual([0,0,0,0])
+  });
+  test(`returns correct combination for large numbers`, () => {
+    expect(splitInteger(120))
+    .toEqual([0,0,2,4])
+    expect(splitInteger(188))
+    .toEqual([3,0,1,7])
+    expect(splitInteger(88))
+    .toEqual([3,0,1,3])
+  });
 });
