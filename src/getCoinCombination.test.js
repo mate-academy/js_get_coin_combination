069405475e78648493f7dfa63972dc24243a0292
DDefty <dawid.kogut99@gmail.com>
2024-07-05 08:01:43 +0200

    Fixed

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,32 +7,32 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
-  test(`returns correct combination for 1 cent`, () => {
-      expect(getCoinCombination(1))
-      .toEqual([1,0,0,0])
+  it(`returns correct combination for 1 cent`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0])
   });
-  test(`returns correct combination for 6 cent`, () => {
-      expect(splitInteger(6))
-      .toEqual([1,1,0,0])
+  it(`returns correct combination for 6 cent`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0])
   });
-  test(`returns correct combination for 17 cent`, () => {
-      expect(splitInteger(17))
-      .toEqual([2,1,1,0])
+  it(`returns correct combination for 17 cent`, () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0])
   });
-  test(`returns correct combination for 50 cent`, () => {
-      expect(splitInteger(50))
-      .toEqual([0,0,0,2])
+  it(`returns correct combination for 50 cent`, () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2])
   });
-  test(`returns correct combination for 0 cent`, () => {
-    expect(splitInteger(0))
-    .toEqual([0,0,0,0])
+  it(`returns correct combination for 0 cent`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0])
   });
-  test(`returns correct combination for large numbers`, () => {
-    expect(splitInteger(120))
-    .toEqual([0,0,2,4])
-    expect(splitInteger(188))
-    .toEqual([3,0,1,7])
-    expect(splitInteger(88))
-    .toEqual([3,0,1,3])
+  it(`returns correct combination for large numbers`, () => {
+    expect(getCoinCombination(120))
+      .toEqual([0, 0, 2, 4])
+    expect(getCoinCombination(188))
+      .toEqual([3, 0, 1, 7])
+    expect(getCoinCombination(88))
+      .toEqual([3, 0, 1, 3])
   });
 });
