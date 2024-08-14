422bde929c8f8c8fd1f09a7d3821eab22fb00688
boytrend0108 <boytrend@gmail.com>
2024-08-14 16:21:07 +0300

    complete

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,29 @@
-'use strict';
+"use strict";
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+describe("getCoinCombination", () => {
+  const { getCoinCombination } = require("./getCoinCombination");
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`should work with 0 centes`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should work when cent > 0 and < 5`, () => {
+    expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
+  });
+
+  it(`should work when cent > 5 and < 10`, () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+  });
+
+  it(`should work when cent > 10 and < 25`, () => {
+    expect(getCoinCombination(15)).toEqual([0, 1, 1, 0]);
+  });
+
+  it(`should work when cent > 25`, () => {
+    expect(getCoinCombination(26)).toEqual([1, 0, 0, 1]);
   });
 });
