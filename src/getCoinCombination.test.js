54d1d64c8430f5e533906f5f0c3621b6d6240b10
haduigon <haduigon@gmail.com>
2024-10-21 16:04:09 +0300

    fixed commit

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,37 +8,37 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`should return coorect combination for 2 cents`, () => {
+  it(`should return correct combination for 2 cents`, () => {
     const result = getCoinCombination(2);
 
     expect(result).toEqual([2, 0, 0, 0]);
   });
 
-  it(`should return coorect combination for 25 cents`, () => {
+  it(`should return correct combination for 25 cents`, () => {
     const result = getCoinCombination(25);
 
     expect(result).toEqual([0, 0, 0, 1]);
   });
 
-  it(`should return coorect combination for 99 cents`, () => {
+  it(`should return correct combination for 99 cents`, () => {
     const result = getCoinCombination(99);
 
     expect(result).toEqual([4, 0, 2, 3]);
   });
 
-  it(`should return coorect combination for 99.8 cents`, () => {
+  it(`should return correct combination for 99.8 cents`, () => {
     const result = getCoinCombination(99.8);
 
     expect(result).toEqual([4, 0, 2, 3]);
   });
 
-  it(`should return seros for 0 cents`, () => {
+  it(`should return zeros for 0 cents`, () => {
     const result = getCoinCombination(0);
 
     expect(result).toEqual([0, 0, 0, 0]);
   });
 
-  it(`should return zeros combination for -cents`, () => {
+  it(`should return zeros combination for negative cents'`, () => {
     const result = getCoinCombination(-100);
 
     expect(result).toEqual([0, 0, 0, 0]);
