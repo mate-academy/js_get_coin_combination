eb00c0248d0db621d4dc41d1a89f8a9b662b5dfc
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-22 16:59:08 +0300

    getCoinCombination function testing

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -6,6 +6,14 @@
  * @returns {number[]}
  */
 function getCoinCombination(cents) {
+  if (cents <= 0) {
+    throw new Error('Please enter number of cents');
+  }
+
+  if (typeof cents !== 'number' || isNaN(cents) || !isFinite(cents)) {
+    throw new Error('Invalid input: must be a finite number');
+  }
+
   let currentAmount = cents;
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
