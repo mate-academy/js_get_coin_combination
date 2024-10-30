7e2a52db73f97da0b2b5f279c2ad48e91f3cb93d
Roma <roma150269@gmail.com>
2024-10-30 12:19:09 +0200

    Solutin

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,28 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should be return '[1, 1, 0, 0]' when argument '6'`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should be return '[0, 0, 0, 2]' when argument '50'`, () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should be return '[2, 1, 1, 0]' when argument '17'`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should be return '[, 0, 0, 0]' when argument '1'`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
 });
