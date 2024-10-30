2a9d7efc4771b3905ce3fcfeb0b6b1af18ddab05
Roma <roma150269@gmail.com>
2024-10-30 12:25:59 +0200

    correct

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,25 +8,25 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  it(`should be return '[1, 1, 0, 0]' when argument '6'`, () => {
+  it(`should return '[1, 1, 0, 0]' when argument is '6'`, () => {
     const result = getCoinCombination(6);
 
     expect(result).toEqual([1, 1, 0, 0]);
   });
 
-  it(`should be return '[0, 0, 0, 2]' when argument '50'`, () => {
+  it(`should return '[0, 0, 0, 2]' when argument is '50'`, () => {
     const result = getCoinCombination(50);
 
     expect(result).toEqual([0, 0, 0, 2]);
   });
 
-  it(`should be return '[2, 1, 1, 0]' when argument '17'`, () => {
+  it(`should return '[2, 1, 1, 0]' when argument is '17'`, () => {
     const result = getCoinCombination(17);
 
     expect(result).toEqual([2, 1, 1, 0]);
   });
 
-  it(`should be return '[, 0, 0, 0]' when argument '1'`, () => {
+  it(`should return '[, 0, 0, 0]' when argument is '1'`, () => {
     const result = getCoinCombination(1);
 
     expect(result).toEqual([1, 0, 0, 0]);
