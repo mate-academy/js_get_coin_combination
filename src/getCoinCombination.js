42db59bfff52de41627a396a0498c197ca17f614
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-11-02 16:25:15 +0200

    tests for getCoinCombination were created

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -6,13 +6,15 @@
  * @returns {number[]}
  */
 function getCoinCombination(cents) {
-  let currentAmount = cents;
+  let currentAmount = Math.trunc(cents);
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
 
-  for (let i = 3; i >= 0; i--) {
-    coins[i] = Math.floor(currentAmount / values[i]);
-    currentAmount -= coins[i] * values[i];
+  if (cents >= 0) {
+    for (let i = 3; i >= 0; i--) {
+      coins[i] = Math.floor(currentAmount / values[i]);
+      currentAmount -= coins[i] * values[i];
+    }
   }
 
   return coins;
