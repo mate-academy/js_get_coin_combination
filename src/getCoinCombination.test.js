5b4b87765990ee44e51caf53c7697f9aa788115f
Alona Riznychenko <alona@chaiz.com>
2024-11-06 21:11:27 +0200

    add task solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,36 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return array from 4 elements`, () => {
+    const result = getCoinCombination(0);
+
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(4);
+  });
+
+  it(`should return right number of pennies`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return right number of pennies and nickels`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return right number of pennies, nickels and dimes`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return right number of pennies, nickels, dimes and quarters`,
+    () => {
+      const result = getCoinCombination(67);
+
+      expect(result).toEqual([2, 1, 1, 2]);
+    });
 });
