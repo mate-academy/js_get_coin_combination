03bd7387d6cbe78ea15e6c49362dc9fa4310d267
Kate Marchuk <pion3219@gmail.com>
2024-11-11 11:22:31 +0200

    Fixed test for 6 cents

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -15,8 +15,8 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it('should return [0, 1, 0, 0] for 6 cents', () => {
-    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  it('should return [1, 1, 0, 0] for 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
   it('should return [0, 0, 1, 0] for 10 cents', () => {
