5d490bfb47f41cea48c91112362a89f4bc90f4b1
dddddd <153018209+Jakdet@users.noreply.github.com>
2024-11-13 19:51:42 +0100

    Update getCoinCombination.test.js

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -13,15 +13,15 @@ describe('getCoinCombination', () => {
   });
 
   it('should operate on 4 places in array', () => {
-    expect(getCoinCombination(172242)).toEqual([2, 1, 1, 6889])
+    expect(getCoinCombination(172242)).toEqual([2, 1, 1, 6889]);
   });
 
   it('should return 0 when 0 < cent < 1', () => {
-    expect(getCoinCombination(1/3)).toEqual([0, 0, 0, 0])
+    expect(getCoinCombination(1/3)).toEqual([0, 0, 0, 0]);
   });
 
-it('should return smalest possible amount of coins', () => {
-  expect(getCoinCombination(25)).toEqual([0, 0, 0, 1])
+it('should return smallest possible amount of coins', () => {
+  expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
 });
 
 });
