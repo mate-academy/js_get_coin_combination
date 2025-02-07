50d76a5a11a47e8f268467485e0a2ed3852eac33
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-02-07 16:38:48 +0400

    add solution

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -10,6 +10,14 @@ function getCoinCombination(cents) {
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
 
+  if (cents < 0) {
+    return coins;
+  }
+
+  if (cents === Infinity) {
+    throw new Error('Please indicate finite number of cents');
+  }
+
   for (let i = 3; i >= 0; i--) {
     coins[i] = Math.floor(currentAmount / values[i]);
     currentAmount -= coins[i] * values[i];
--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('the result is array of 0 when "cents" === 0', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('the result is array of 0 when "cents" < 0', () => {
+    expect(getCoinCombination(-Infinity)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should correctly distribute money when "cents" === 9', () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+  });
+
+  it('should correctly distribute money when "cents" === 116', () => {
+    expect(getCoinCombination(116)).toEqual([1, 1, 1, 4]);
+  });
+
+  it('"cents" should be a finite number', () => {
+    expect(() => getCoinCombination(Infinity)).toThrow();
+  });
 });
