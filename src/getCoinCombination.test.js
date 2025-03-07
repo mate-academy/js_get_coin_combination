b320b5a6ee1025cff36ef541d7b22e9dbc475d9e
Kernel481 <kernel481@gmail.com>
2025-03-07 17:06:15 +0200

    Combo test

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,34 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return 1 for 1`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return 1 for 5`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return 1 for 5`, () => {
+    expect(getCoinCombination(5))
+      .toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return 1 for 10`, () => {
+    expect(getCoinCombination(10))
+      .toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return 1 for 25`, () => {
+    expect(getCoinCombination(25))
+      .toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return 1 for 41`, () => {
+    expect(getCoinCombination(41))
+      .toEqual([1, 1, 1, 1]);
+  });
 });
