43fea6268c45e1d84686f910edd15213bba034fd
Kiborg1-git <hibro867@gmail.com>
2025-03-08 11:31:42 +0200

    Added tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('Input 1 return [1, 0, 0, 0] cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('Input 6 return [1, 1, 0, 0] cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('Input 17 return [2, 1, 1, 0] cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('Input 50 return [0, 0, 0, 2] cents', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
