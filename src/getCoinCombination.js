af3badd0703a4ee1f86391e7c735b4fdf5b999b1
ViktorMironik <viktor36vmm@gmail.com>
2025-03-09 22:24:56 +0200

    Fix coin combination logic to return the count of each coin type

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -1,14 +1,14 @@
 'use strict';
 
 function getCoinCombination(amount) {
-  const coins = [50, 20, 10, 5, 2, 1];
-  const result = [];
+  const coinTypes = [25, 10, 5, 1]; // Penny, Nickel, Dime, Quarter
+  const result = [0, 0, 0, 0]; // [quarters, dimes, nickels, pennies]
   let remainingAmount = amount;
 
-  for (let i = 0; i < coins.length; i++) {
-    while (remainingAmount >= coins[i]) {
-      result.push(coins[i]);
-      remainingAmount -= coins[i];
+  for (let i = 0; i < coinTypes.length; i++) {
+    while (remainingAmount >= coinTypes[i]) {
+      remainingAmount -= coinTypes[i];
+      result[i]++;
     }
   }
 
--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,9 +3,11 @@
 const getCoinCombination = require('./getCoinCombination');
 
 test('should return correct coin combination for 87', () => {
-  expect(getCoinCombination(87)).toEqual([50, 20, 10, 5, 2]);
+  expect(getCoinCombination(87))
+    .toEqual([3, 1, 0, 2]); // [3 Quarters, 1 Dime, 0 Nickels, 2 Pennies]
 });
 
 test('should return correct coin combination for 99', () => {
-  expect(getCoinCombination(99)).toEqual([50, 20, 20, 5, 2, 2]);
+  expect(getCoinCombination(99))
+    .toEqual([3, 2, 0, 4]); // [3 Quarters, 2 Dimes, 0 Nickels, 4 Pennies]
 });
