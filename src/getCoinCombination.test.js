44ec39d953611aed4c0dd731ff8a8e88e266c630
Vladyslav <vladbuldenko@gmail.com>
2025-03-23 16:03:08 +0200

    VB solution QA

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,26 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`should return an array with pennies`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return an array with nickels`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return an array with dimes`, () => {
+    expect(getCoinCombination(16)).toEqual([1, 1, 1, 0]);
+  });
+
+  it(`should return an array with quaters`, () => {
+    expect(getCoinCombination(100)).toEqual([0, 0, 0, 4]);
+  });
+
+  it(`should calculate all coins`, () => {
+    expect(getCoinCombination(116)).toEqual([1, 1, 1, 4]);
   });
 });
