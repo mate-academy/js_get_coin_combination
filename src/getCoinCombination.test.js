e281145362dee17fa5fef64ae5c785faa1492df2
Vladyslav <vladbuldenko@gmail.com>
2025-03-23 16:12:32 +0200

    VB solution QA

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+describe("getCoinCombination", () => {
+  const { getCoinCombination } = require("./getCoinCombination");
 
   it(`should be declared`, () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
@@ -25,5 +25,17 @@ describe('getCoinCombination', () => {
 
   it(`should calculate all coins`, () => {
     expect(getCoinCombination(116)).toEqual([1, 1, 1, 4]);
+  });
+
+  it(`should return 0`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return 2 quaters`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should calculate all coins except quaters`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 });
