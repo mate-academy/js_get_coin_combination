11714eb5c3e529e0e7942dd71038af79f01bf905
Valeriia Kozmenko <leraz9953@gmail.com>
2025-03-24 16:01:35 +0200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -35,8 +35,8 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
   });
 
-  it('should return [1, 1, 0, 0] for 6 cents (boundary after 5 cents)', () => {
-    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  it('should return[4, 1, 0, 0]for 9 cents (boundary forb 9-cent coin)', () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
   });
 
   it('should return[0, 0, 1, 0]for 10 cents(boundary for 10-cent coin)', () => {
@@ -45,6 +45,10 @@ describe('getCoinCombination', () => {
 
   it('should return[0, 0, 2, 0]for 20cents(boundary for 10-cent coins)', () => {
     expect(getCoinCombination(20)).toEqual([0, 0, 2, 0]);
+  });
+
+  it('should return[4, 0, 2, 0]for 24 cents(boundary for 24-cent coin)', () => {
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
   });
 
   it('should return[0, 0, 0, 1]for 25 cents(boundary for 25-cent coin)', () => {
