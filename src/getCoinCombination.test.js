2d7289f0e33e9b2e281120771d8915c3edd43c2c
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-04-14 22:50:52 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test('returns [1, 0, 0, 0] when 1 p is passed as an argunment', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  test('returns [1, 1, 0, 0] when 6 p is passed as an argunment', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  test('returns [2, 1, 1, 0] when 17 p is passed as an argunment', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  test('returns [0, 0, 0, 2] when 50 p is passed as an argunment', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
