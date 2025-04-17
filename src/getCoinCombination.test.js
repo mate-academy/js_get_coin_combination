8592a20420c41a7d343313d9f8d6177521f2bf3a
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-17 18:49:31 +0300

    d

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test('Should return [1, 0, 0, 0] for 1', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  test('Should return [1, 1, 0, 0] for 6', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  test('Should return [2, 1, 1, 0] for 17', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  test('Should return [0, 0, 0, 2] for 50', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
