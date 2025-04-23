f494a9092677c20eceb749f61071335effe7b193
Janani <jananiraja1405@gmail.com>
2025-04-23 12:48:28 +1200

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return only for Quarters ', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it('should return only for Penny', () => {
+    expect(getCoinCombination(2)).toEqual([2, 0, 0, 0]);
+  });
+
+  it('should return for Penny and Nickel', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return for Penny, Nickel and Dime', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should have combination of all', () => {
+    expect(getCoinCombination(42)).toEqual([2, 1, 1, 1]);
+  });
 });
