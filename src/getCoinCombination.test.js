324ffd3b5e35c85cfa891e7a53dab3b5401360c1
yura-pepsicola <y.nosuch@gmail.com>
2025-05-27 17:52:34 +0300

    added unit tests for getCoinCombination()

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,36 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return array with 0's if cents === 0`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return [4,0,0,0] if cents === 4`, () => {
+    expect(getCoinCombination(4)).toEqual([4, 0, 0, 0]);
+  });
+
+  it(`should return [0,1,0,0] if cents === 5`, () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return [4,1,0,0] if cents === 9`, () => {
+    expect(getCoinCombination(9)).toEqual([4, 1, 0, 0]);
+  });
+
+  it(`should return [0,0,1,0] if cents === 10`, () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [4,0,2,0] if cents === 24`, () => {
+    expect(getCoinCombination(24)).toEqual([4, 0, 2, 0]);
+  });
+
+  it(`should return [0,0,0,1] if cents === 25`, () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should throw an error if the value is not a number`, () => {
+    expect(getCoinCombination(-5)).toThrow();
+  });
 });
