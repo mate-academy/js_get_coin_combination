48f2c3897ac9f397bc3942fffd5d7fe8b1d85050
Stas <stas.mylashenko@gmail.com>
2025-05-27 16:51:16 +0300

    Solution

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * @param {number} cents
+ * @param {number}cents
  *
  * @returns {number[]}
  */
--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,22 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  test(`should return [1, 0, 0, 0 for 1 penny]`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  test(`should return [1, 1, 0, 0 for 6 cents]`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  test(`should return [2, 1, 1, 0 for 17 cents]`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  test(`should return [0, 0, 0, 2 for 50 cents]`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
