8d83a9ef6d99b75850bbd200ade4a08d6976afce
Alyona Stolyarenko <estolarenko5@gmail/com>
2025-05-27 18:09:48 +0300

    Add getCoinCombination tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,27 @@
 'use strict';
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
-  });
+test('1 cent → [1, 0, 0, 0]', () => {
+  expect(getCoinCombination(1)).toStrictEqual([1, 0, 0, 0]);
+});
+
+test('6 cents → [1, 1, 0, 0]', () => {
+  expect(getCoinCombination(6)).toStrictEqual([1, 1, 0, 0]);
+});
+
+test('17 cents → [2, 1, 1, 0]', () => {
+  expect(getCoinCombination(17)).toStrictEqual([2, 1, 1, 0]);
+});
+
+test('50 cents → [0, 0, 0, 2]', () => {
+  expect(getCoinCombination(50)).toStrictEqual([0, 0, 0, 2]);
+});
+
+test('0 cents → [0, 0, 0, 0]', () => {
+  expect(getCoinCombination(0)).toStrictEqual([0, 0, 0, 0]);
+});
+
+test('99 cents → [4, 0, 2, 3]', () => {
+  expect(getCoinCombination(99)).toStrictEqual([4, 0, 2, 3]);
 });
