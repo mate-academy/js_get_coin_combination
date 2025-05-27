c5a2fc066b9e5ec0154e89aa68a437be2deb6556
Viktoriia Voropaieva <dmntvva@gmail.com>
2025-05-27 20:28:05 +0300

    restore changes

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -15,15 +15,9 @@ function getCoinCombination(cents) {
   let currentAmount = cents;
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
-  const arrLength = values.length - 1;
-  // for (let i = 3; i >= 0; i--) {
-  //   coins[i] = Math.floor(currentAmount / values[i]);
-  //   currentAmount -= coins[i] * values[i];
-  // }
-
-  for (let i = 0; i <= arrLength; i++) {
-    coins[arrLength - i] = Math.floor(currentAmount / values[arrLength - i]);
-    currentAmount -= coins[arrLength - i] * values[arrLength - i];
+  for (let i = 3; i >= 0; i--) {
+    coins[i] = Math.floor(currentAmount / values[i]);
+    currentAmount -= coins[i] * values[i];
   }
 
   return coins;
