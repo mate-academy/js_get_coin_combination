c67957309b9d04721ec8a8bad800a09d818dbf71
Vitalii Nezdoimynoha <vitalii.nezdojminoga@gmail.com>
2025-06-19 10:03:42 +0300

    Add test solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,58 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return [0, 0, 0, 0] for 0 cents`, () => {
+    const result = getCoinCombination(0);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return [1, 0, 0, 0] for 1 cents`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return [4, 0, 0, 0] for 4 cents`, () => {
+    const result = getCoinCombination(4);
+
+    expect(result).toEqual([4, 0, 0, 0]);
+  });
+
+  it(`should return [0, 1, 0, 0] for 5 cents`, () => {
+    const result = getCoinCombination(5);
+
+    expect(result).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return [1, 1, 0, 0] for 6 cents`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return [0, 0, 1, 0] for 10 cents`, () => {
+    const result = getCoinCombination(10);
+
+    expect(result).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return [2, 1, 1, 0] for 17 cents`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return [0, 0, 0, 1] for 25 cents`, () => {
+    const result = getCoinCombination(25);
+
+    expect(result).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return [0, 0, 0, 2] for 50 cents`, () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+  });
 });
