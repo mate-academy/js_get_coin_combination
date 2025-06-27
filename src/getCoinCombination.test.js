a0c950c12fa7970636b0b56241dea5920ac8ae7d
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-27 16:38:06 +0300

    res

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -8,8 +8,10 @@ describe('getCoinCombination', () => {
       .toBeInstanceOf(Function);
   });
 
-  expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
-  expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
-  expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
-  expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  test('expect to return right coin combinations', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
