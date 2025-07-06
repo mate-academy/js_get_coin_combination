b439e42b3dc448d9d821125cfb3617d091aabe81
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2025-07-06 15:22:54 +0200

    add task solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,22 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('should return correct value', () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return array of zeros if value = 0', () => {
+    const result = getCoinCombination(0);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should calculate big values correctly', () => {
+    const result = getCoinCombination(101);
+
+    expect(result).toEqual([1, 0, 0, 4]);
+  });
 });
