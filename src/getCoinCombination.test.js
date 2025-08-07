eb1aab1f23ecfa82f2e4f4631666f693e6dc0180
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-08-07 21:12:21 +0300

    finish the task

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,28 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('breaks down 7 cents into small coins correctly', () => {
+    expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
+  });
+
+  it('uses a dime and a penny for 11 cents', () => {
+    expect(getCoinCombination(11)).toEqual([1, 0, 1, 0]);
+  });
+
+  it('combines 1¢ and 25¢ for total of 26', () => {
+    expect(getCoinCombination(26)).toEqual([1, 0, 0, 1]);
+  });
+
+  it('constructs 39 cents using one of each coin type', () => {
+    expect(getCoinCombination(39)).toEqual([4, 0, 1, 1]);
+  });
+
+  it('distributes 75 cents into three 25¢ coins', () => {
+    expect(getCoinCombination(75)).toEqual([0, 0, 0, 3]);
+  });
+
+  it('splits 50 cents into two 25-cent coins', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
 });
