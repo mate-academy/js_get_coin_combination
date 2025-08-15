e3fcb4910e3c287f882e958a11051d0134da7f31
damian Buń <damian.bun00@gmail.com>
2025-08-15 17:44:05 +0200

    Solution of the task

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,24 @@
 'use strict';
 
 describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+  const {
+    getCoinCombination
+  } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
+  });
+
+  it('should return expected values for 0 cents', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return expected values for non-negative integer cents', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it('should return expected values for non-negative integer cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 });
