a77d253d76540760e9da82a823ed9669642913de
damian Buń <damian.bun00@gmail.com>
2025-08-16 19:20:15 +0200

    solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,7 +3,7 @@
 describe('getCoinCombination', () => {
   const {
     getCoinCombination,
-  } = require('./getCoinCombination');
+  } = require('./getCoinCombination.js');
 
   it(`should be declared`, () => {
     expect(getCoinCombination)
