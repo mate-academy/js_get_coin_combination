cdcf2bceb834449b77f3af37c11e1e7b6c92e85f
Eliza Dubiel <dubieleliza89@gmail.com>
2025-08-17 11:57:29 +0200

    Coin

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -1,26 +1,18 @@
 'use strict';
 
-/**
- * @param {number} cents
- * @returns {number[]}
- */
 function getCoinCombination(cents) {
   let currentAmount = cents;
-  const coins = [0, 0, 0, 0]; // pennies, nickels, dimes, quarters
+  const coins = [0, 0, 0, 0];
 
-  // quarters
   coins[3] = Math.floor(currentAmount / 25);
   currentAmount -= coins[3] * 25;
 
-  // dimes
   coins[2] = Math.floor(currentAmount / 10);
   currentAmount -= coins[2] * 10;
 
-  // nickels
   coins[1] = Math.floor(currentAmount / 5);
   currentAmount -= coins[1] * 5;
 
-  // pennies
   coins[0] = currentAmount;
 
   return coins;
