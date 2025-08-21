2a9ae583ca5a15eca4fb833d4efee9d6fe1bd94e
Eater228 <543145yura@gmail.com>
2025-08-21 18:54:27 +0300

    add solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,16 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it('retun [0,0,0,2] for 50', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it('retun [0, 0, 0, 0] for 0', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return an array of four numbers', () => {
+    expect(getCoinCombination(99)).toHaveLength(4);
+  });
 });
