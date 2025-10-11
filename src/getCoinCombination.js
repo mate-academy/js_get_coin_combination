fa9480a553997a5370a5e61e7a8b56db1296e452
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2025-10-11 20:41:47 +0300

    change  tests for getCombination  function

--- src/getCoinCombination.js
+++ src/getCoinCombination.js
@@ -9,10 +9,6 @@ function getCoinCombination(cents) {
   let currentAmount = cents;
   const values = [1, 5, 10, 25];
   const coins = [0, 0, 0, 0];
-  
-  if (cents <= 0) {
-    return coins;
-  }
 
   for (let i = 3; i >= 0; i--) {
     coins[i] = Math.floor(currentAmount / values[i]);
