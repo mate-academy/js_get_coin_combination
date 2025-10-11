65de639ac6488a76f343fa3a952595d73b1b6bb9
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2025-10-11 20:12:42 +0300

    Add tests for getCombination  function

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,44 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return correct result for 0`, () => {
+    expect(getCoinCombination(0))
+      .toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return correct result for 1`, () => {
+    expect(getCoinCombination(1))
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return correct result for 6`, () => {
+    expect(getCoinCombination(6))
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return correct result for 16`, () => {
+    expect(getCoinCombination(16))
+      .toEqual([1, 1, 1, 0]);
+  });
+
+  it(`should return correct result for 31`, () => {
+    expect(getCoinCombination(31))
+      .toEqual([1, 1, 0, 1]);
+  });
+
+  it(`should return correct result for 99`, () => {
+    expect(getCoinCombination(99))
+      .toEqual([4, 0, 2, 3]);
+  });
+
+  it(`should return correct result for 100`, () => {
+    expect(getCoinCombination(100))
+      .toEqual([0, 0, 0, 4]);
+  });
+
+  it(`should return correct result for -1`, () => {
+    expect(getCoinCombination(-1))
+      .toEqual([0, 0, 0, 0]);
+  });
 });
