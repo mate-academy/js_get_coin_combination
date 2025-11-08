07b4e457340f00439615c998184b015efa6f7426
Mykhailo <kurochkinmy@gmail.com>
2025-11-08 16:14:25 +0100

    feat: implement correct tests for getCoinCombination function

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -2,9 +2,28 @@
 
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
+  const cases = [
+    {
+      input: 1, expected: [1, 0, 0, 0],
+    },
+    {
+      input: 6, expected: [1, 1, 0, 0],
+    },
+    {
+      input: 17, expected: [2, 1, 1, 0],
+    },
+    {
+      input: 50, expected: [0, 0, 0, 2],
+    },
+  ];
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  cases.forEach(({ input, expected }) => {
+    it(`should work correctly with ${input} 'cents'`, () => {
+      expect(getCoinCombination(input)).toEqual(expected);
+    });
   });
 });
