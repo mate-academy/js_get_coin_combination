f9db0f9013f47d97751e643beb0b1c151f527b1e
Rymma-MAC <rymma-mac@MacBook-Air-Rymma-MAC.local>
2025-11-09 16:14:38 +0200

    add tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,39 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return correct combination for 1 cent`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result)
+      .toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return correct combination for 6 cent`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result)
+      .toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return correct combination for 17 cent`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result)
+      .toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return correct combination for 50 cent`, () => {
+    const result = getCoinCombination(50);
+
+    expect(result)
+      .toEqual([0, 0, 0, 2]);
+  });
+
+  it(`should return correct combination for 0 cent`, () => {
+    const result = getCoinCombination(0);
+
+    expect(result)
+      .toEqual([0, 0, 0, 0]);
+  });
 });
