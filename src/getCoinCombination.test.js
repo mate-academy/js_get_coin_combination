f50d5ef025d7aea0ec6f4be69317a6786efceb97
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-10 20:59:16 +0000

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,24 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  it(`should return an array`, () => {
+    const result = getCoinCombination(100, [1, 5, 10, 25, 50]);
+
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  it(`should return correct coin combination`, () => {
+    const totalCoins = 63;
+    const result = getCoinCombination(totalCoins);
+
+    expect(result).toEqual([3, 0, 1, 2]);
+  });
+
+  it(`should return correct coin combination for zero cents`, () => {
+    const totalCoins = 0;
+    const result = getCoinCombination(totalCoins);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
 });
