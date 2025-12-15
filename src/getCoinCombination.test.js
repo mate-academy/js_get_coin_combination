b2942a5495f9c2041763c6c6f460c9aae22c3450
Serhii-Khobotov <hobbitura@gmail.com>
2025-12-15 18:57:19 +0200

    my sollution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,4 +7,20 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination)
       .toBeInstanceOf(Function);
   });
+
+  test('should return array', () => {
+    expect(getCoinCombination(50)).toBeInstanceOf(Array)
+  })
+
+  test('should return [0, 0, 0, 0] if cents === 0', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  })
+
+  test('should return array with length === 4', () => {
+    expect(getCoinCombination(50).length).toBe(4);
+  })
+
+  test('should return [0, 1, 1, 3] if cents === 90', () => {
+    expect(getCoinCombination(90)).toEqual([0, 1, 1, 3]);
+  });
 });
