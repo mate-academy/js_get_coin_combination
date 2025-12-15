3a42377723d0bc93bcd02ae8bdda808181bccf82
Serhii-Khobotov <hobbitura@gmail.com>
2025-12-15 19:14:58 +0200

    test for 1, 6, 17, 50 was added

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,18 +9,34 @@ describe('getCoinCombination', () => {
   });
 
   test('should return array', () => {
-    expect(getCoinCombination(50)).toBeInstanceOf(Array)
-  })
+    expect(getCoinCombination(50)).toBeInstanceOf(Array);
+  });
 
   test('should return [0, 0, 0, 0] if cents === 0', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
-  })
+  });
 
   test('should return array with length === 4', () => {
     expect(getCoinCombination(50).length).toBe(4);
-  })
+  });
 
   test('should return [0, 1, 1, 3] if cents === 90', () => {
     expect(getCoinCombination(90)).toEqual([0, 1, 1, 3]);
+  });
+
+  test('should return [1, 0, 0, 0] if cents === 1', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  test('should return [1, 1, 0, 0] if cents === 6', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  test('should return [2, 1, 1, 0] if cents === 17', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  test('should return [0, 0, 0, 2] if cents === 50', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
