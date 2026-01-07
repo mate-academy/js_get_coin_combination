6c522b58d68b41a81d7fd1b2103e9e34f3332615
Dmytro <dimusgamer2.0@gmail.com>
2026-01-07 04:25:30 +0200

    add missing tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -28,9 +28,19 @@ describe('getCoinCombination', () => {
       .toEqual([1, 1, 1, 0]);
   });
 
+  it('17 cents = 1 dime + 1 nickel + 2 penny = [2, 1, 1, 0]', () => {
+    expect(getCoinCombination(17))
+      .toEqual([2, 1, 1, 0]);
+  });
+
   it('31 cents = 1 quarter + 1 nickel + 1 penny = [1, 1, 0, 1]', () => {
     expect(getCoinCombination(31))
       .toEqual([1, 1, 0, 1]);
+  });
+
+  it('50 cents = 2 quarters = [0, 0, 0, 2]', () => {
+    expect(getCoinCombination(50))
+      .toEqual([0, 0, 0, 2]);
   });
 
   it('99 cents = 3 quarters + 2 dimes + 4 pennies = [4, 0, 2, 3]', () => {
