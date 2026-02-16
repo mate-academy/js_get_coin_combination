8503ee08a484e162255d0cc6ddc75273a6ff2594
Vitalii <v.hulaievych@gmail.com>
2026-02-16 22:35:13 +0000

    solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+describe("getCoinCombination", () => {
+  const { getCoinCombination } = require("./getCoinCombination");
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it("should return an array with right length", () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+    expect(result).toHaveLength(4);
+  });
+
+  it("should return right results", () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 });
