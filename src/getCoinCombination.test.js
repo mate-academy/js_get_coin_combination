3b102957f9deb0b05e7ad334dad29cd2aea66c48
Svitlana Chepa <svetachepa2320@gmail.com>
2026-02-19 09:57:56 +0000

    Fix solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -9,15 +9,15 @@ describe('getCoinCombination', () => {
   });
 
   it('number of pennies (1 penny = 1 cent)', () => {
-    const result = getCoinCombination(5);
-
-    expect(result).toEqual([0, 1, 0, 0]);
-  });
-
-  it(' number of nickels (1 nickel = 5 cents)', () => {
     const result = getCoinCombination(1);
 
     expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it(' number of nickels (1 nickel = 5 cents)', () => {
+    const result = getCoinCombination(5);
+
+    expect(result).toEqual([0, 5, 0, 0]);
   });
 
   it('number of dimes (1 dime = 10 cents)', () => {
