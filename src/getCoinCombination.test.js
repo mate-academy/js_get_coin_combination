ac3f8a1573e50d8ae3692edf78ea43e2a9d2d83a
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-27 11:01:01 +0200

    Add solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,38 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it('should count number of pennies correctly', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should count number of nickels correctly', () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it('should count number of pennies and nickels correctly', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should count number of dimes correctly', () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it('should count number of pennies, nickels and dimes correctly', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should count number of quarters correctly', () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it('should count number of all coin types correctly', () => {
+    expect(getCoinCombination(36)).toEqual([1, 0, 1, 1]);
+  });
+
+  it('should count number of quarters correctly', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
