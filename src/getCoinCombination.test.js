921a3930858ac37b6933cd1d0415d4153da26dd6
Vital <vitaligna@gmail.com>
2026-03-07 11:43:24 +0200

    add solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,30 @@
+/* eslint-disable quotes */
 'use strict';
 
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`return 0 if 0 cents`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`return 1 penni if 1 cent`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`return 1 penni, 1 nickel if 6 cents`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`return 2 pennies, 1 nickel, 1 dime if 17 cents`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`return 2 quarters if 50 cents`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
