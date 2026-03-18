93e8fdfe1024fbd7b52e190a86cf002ca7472522
Igor <igor.olshanskyi@gmail.com>
2026-03-18 11:29:50 +0200

    solve

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -3,7 +3,19 @@
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
-    expect(getCoinCombination).toBeInstanceOf(Function);
+  it(`if 1 coin should be 1 penny`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`if 6 coin should be 1 penny + 1 nickel`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`if 17 coin should be 2 pennies + 1 nickel + 1 dime`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`if 50 coin should be 2 quarters`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
