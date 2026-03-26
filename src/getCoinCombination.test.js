02e2fe15df43e54627bfacc18a0116a6468e10b2
Olena <olenahorodn7202@gmail.com>
2026-03-26 19:56:52 +0200

    solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,9 +1,33 @@
 'use strict';
 
-describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
+const { getCoinCombination } = require('./getCoinCombination');
 
-  it(`should be declared`, () => {
+describe('getCoinCombination', () => {
+  it('should be declared', () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it('should return correct coins for 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it('should return correct coins for 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
+  });
+
+  it('should return correct coins for 17 cents', () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
+  });
+
+  it('should return correct coins for 50 cents', () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
+  });
+
+  it('should return correct coins for 99 cents', () => {
+    expect(getCoinCombination(99)).toEqual([4, 0, 2, 3]);
+  });
+
+  it('should return all zeros for 0 cents', () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 });
