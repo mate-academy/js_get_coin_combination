b2b7961cb4ba587d5b04fd485fd511174ee958ce
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-16 15:30:22 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,9 +1,32 @@
 'use strict';
+const { getCoinCombination } = require('./getCoinCombination');
 
 describe('getCoinCombination', () => {
-  const { getCoinCombination } = require('./getCoinCombination');
-
   it(`should be declared`, () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`should works without cents`, () => {
+    expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return only penni`, () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return only nickel`, () => {
+    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  });
+
+  it('should return only dime', () => {
+    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  });
+
+  it('should return only quarter', () => {
+    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  });
+
+  it('should correct divide cents', () => {
+    expect(getCoinCombination(41)).toEqual([1, 1, 1, 1]);
   });
 });
