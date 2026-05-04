e9ded832d6bb72750575972031b6262814b75f64
Alyona Shunevych <olena17ju06@gmail.com>
2026-05-04 20:55:41 +0300

    Solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -6,4 +6,42 @@ describe('getCoinCombination', () => {
   it(`should be declared`, () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
+
+  it(`should only return 'pennies' if 'cents' < 5`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should only return 'nickels' if 'cents' === 5`, () => {
+    const result = getCoinCombination(5);
+
+    expect(result).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return 'pennies' + 'nickels' if 5 < 'cents' < 10`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should only return 'dimes' if 'cents' equal 10 or 20`, () => {
+    const result1 = getCoinCombination(10);
+    const result2 = getCoinCombination(20);
+
+    expect(result1).toEqual([0, 0, 1, 0]);
+    expect(result2).toEqual([0, 0, 2, 0]);
+  });
+
+  it(`should return 'pennies' + 'nickels' + 'dimes' if 10 < 'cents' < 25`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should only return 'quarters' if 'cents' is a multiple of 25`, () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
+  });
 });
