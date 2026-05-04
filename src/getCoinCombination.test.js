53567ca6b2a38b2413da0b52b24df1e7446dd9f8
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-05-04 13:02:09 +0300

    fix description

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -7,23 +7,23 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
 
-  it(`should be one 'penny' if there is 1 'cent'`, () => {
+  it(`should be 1 'penny' if there is 1 'cent'`, () => {
     expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it(`should be one 'nickel' if there is 5 'cents'`, () => {
-    expect(getCoinCombination(5)).toEqual([0, 1, 0, 0]);
+  it(`should be 1 'penny' and 1 'nickel' if there is 6 'cents'`, () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it(`should be one 'dime' if there is 10 'cents'`, () => {
-    expect(getCoinCombination(10)).toEqual([0, 0, 1, 0]);
+  it(`should be 2 'pennies', 1 'nickel' and 1 'dime' if there is 17 'cents'`, () => {
+    expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it(`should be one 'quarter' if there is 25 'cents'`, () => {
-    expect(getCoinCombination(25)).toEqual([0, 0, 0, 1]);
+  it(`should be 2 'quarters' if there is 50 'cents'`, () => {
+    expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 
-  it(`should be 2 'pennies', 1 'nickel', 1 'dime' and 2 'quarters' if there is 76 'cents'`, () => {
+  it(`should be 2 'pennies', 1 'nickel', 1 'dime' and 2 'quarters' if there is 67 'cents'`, () => {
     expect(getCoinCombination(67)).toEqual([2, 1, 1, 2]);
   });
 });
