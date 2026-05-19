693973db909ade287bcfcf6940efe241aef44754
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2026-05-19 23:47:03 +0300

    add task solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
 describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
+    
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
 });
