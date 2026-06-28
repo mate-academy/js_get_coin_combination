c9e48252b6afeec2d03960cba9a2dba333a391c3
Ivan <kovalevivan420@gmail.com>
2026-06-28 17:08:11 +0200

    fixing according mentor's requirements

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -15,23 +15,19 @@ describe('getCoinCombination', () => {
     expect(getCoinCombination(0)).toEqual([0, 0, 0, 0]);
   });
 
-  it(`'cents' < 5 changes only coins[0]`, () => {
-    expect(getCoinCombination(3)).toEqual([3, 0, 0, 0]);
+  it('should return [1, 0, 0, 0] for 1 cent', () => {
+    expect(getCoinCombination(1)).toEqual([1, 0, 0, 0]);
   });
 
-  it(`'cents' < 10 changes coins[0] and coins[1]`, () => {
-    expect(getCoinCombination(7)).toEqual([2, 1, 0, 0]);
+  it('should return [1, 1, 0, 0] for 6 cents', () => {
+    expect(getCoinCombination(6)).toEqual([1, 1, 0, 0]);
   });
 
-  it(`'cents' < 25 changes coins[0], coins[1] and coins[2]`, () => {
+  it('should return [2, 1, 1, 0] for 17 cents', () => {
     expect(getCoinCombination(17)).toEqual([2, 1, 1, 0]);
   });
 
-  it(`'cents' >= 25 influence on all 'coins' indexes`, () => {
-    expect(getCoinCombination(67)).toEqual([2, 1, 1, 2]);
-  });
-
-  it(`'cents' is an exact multiple of 25 — only quarters used`, () => {
+  it('should return [0, 0, 0, 2] for 50 cents', () => {
     expect(getCoinCombination(50)).toEqual([0, 0, 0, 2]);
   });
 });
