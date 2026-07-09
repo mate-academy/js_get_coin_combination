f261ea743a374e77be38149f6ab6835b8235065a
Yaroslav <yaroslavhohulia@gmail.com>
2026-07-09 18:53:26 +0300

    added tests

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -6,4 +6,40 @@ describe('getCoinCombination', () => {
   it(`should be declared`, () => {
     expect(getCoinCombination).toBeInstanceOf(Function);
   });
+
+  it(`should return zeros for value of 0`, () => {
+    const result = getCoinCombination(0);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it(`should return one penny for value of 1`, () => {
+    const result = getCoinCombination(1);
+
+    expect(result).toEqual([1, 0, 0, 0]);
+  });
+
+  it(`should return one nickel for value of 5`, () => {
+    const result = getCoinCombination(5);
+
+    expect(result).toEqual([0, 1, 0, 0]);
+  });
+
+  it(`should return one dime for value of 10`, () => {
+    const result = getCoinCombination(10);
+
+    expect(result).toEqual([0, 0, 1, 0]);
+  });
+
+  it(`should return one quarter for value of 25`, () => {
+    const result = getCoinCombination(25);
+
+    expect(result).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return trutly result for value of 42`, () => {
+    const result = getCoinCombination(42);
+
+    expect(result).toEqual([2, 1, 1, 1]);
+  });
 });
